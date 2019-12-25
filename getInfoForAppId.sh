8293f2272f33889b47465932ce949d10c65fa802
Leo Wandersleb <leo@leowandersleb.de>
2019-12-25 19:13:15 +1300

    call getInfo with list of appIds

--- getInfoForAppId.sh
+++ getInfoForAppId.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# run this using Docker: `docker run -it -v$PWD:/mnt node bash /mnt/getInfoForAppId.sh com.mycelium.wallet`
+# run this using Docker:
+# docker run -it -v$PWD:/mnt node bash /mnt/getInfoForAppId.sh com.company1.wallet com.company2.wallet
 
-appId=$1
-echo updating info for app $appId
+for appId in "$@"; do
+  echo updating info for app $appId
 
-npm install google-play-scraper dateformat sync-request
-cd /mnt
+  npm install google-play-scraper dateformat sync-request
+  cd /mnt
 
-node getInfoForAppId.js $appId
+  node getInfoForAppId.js $appId
+done
