9f6089d926c09e92fa4b7674f255182a728ba2d5
Leo Wandersleb <leo@leowandersleb.de>
2019-12-30 13:13:38 +1300

    render fewusers above wip in the list

--- getInfoSanitize.js
+++ getInfoSanitize.js
@@ -99,7 +99,7 @@ repository: ${header.repository || ""}
 issue: ${header.issue || ""}
 icon: ${header.appId}.${iconExtension}
 bugbounty: ${header.bugbounty || ""}
-verdict: ${header.verdict} # May be any of: wip, nowallet, nobtc, custodial, nosource, nonverifiable, verifiable, bounty
+verdict: ${header.verdict} # May be any of: wip, fewusers, nowallet, nobtc, custodial, nosource, nonverifiable, verifiable, bounty
 date: ${dateFormat(header.date, "yyyy-mm-dd")}
 reviewStale: ${stale}
 
