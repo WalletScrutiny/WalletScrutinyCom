76e837323c6a84eec55bdd56948dae7e95a770e4
Leo Wandersleb <leo@leowandersleb.de>
2020-05-13 16:29:02 -0400

    improved page loading

---
+++ updateImages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir images/wallet_icons/tiny/ images/wallet_icons/small/
+for f in $( cd images/wallet_icons; ls *.* )
+do
+  echo $f
+  file="images/wallet_icons/$f"
+  convert -background none "resSources/wallet_icons/$f" -resize 100x images/wallet_icons/small/$f
+  convert -background none "resSources/wallet_icons/$f" -resize 25x images/wallet_icons/tiny/$f
+done
+convert "resSources/hacker.jpg" -resize 200x images/hacker.jpg
