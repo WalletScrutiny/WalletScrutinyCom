529c96692f2f0cd462424c9527f12c205727a583
Leo Wandersleb <leo@leowandersleb.de>
2020-05-21 21:42:59 -0400

    fix updateImages script

--- updateImages.sh
+++ updateImages.sh
@@ -5,7 +5,7 @@ for f in $( cd images/wallet_icons; ls *.* )
 do
   echo $f
   file="images/wallet_icons/$f"
-  convert -background none "resSources/wallet_icons/$f" -resize 100x images/wallet_icons/small/$f
-  convert -background none "resSources/wallet_icons/$f" -resize 25x images/wallet_icons/tiny/$f
+  convert -background none "images/wallet_icons/$f" -resize 100x images/wallet_icons/small/$f
+  convert -background none "images/wallet_icons/$f" -resize 25x images/wallet_icons/tiny/$f
 done
 convert "resSources/hacker.jpg" -resize 200x images/hacker.jpg
