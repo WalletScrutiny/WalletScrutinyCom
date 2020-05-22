a28f93354c3f25724b3924519927487b743a53d8
Leo Wandersleb <leo@leowandersleb.de>
2020-05-21 23:04:21 -0400

    improved thumbnail creation

--- updateImages.sh
+++ updateImages.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-mkdir images/wallet_icons/tiny/ images/wallet_icons/small/
-for f in $( cd images/wallet_icons; ls *.* )
-do
-  echo $f
-  file="images/wallet_icons/$f"
-  convert -background none "images/wallet_icons/$f" -resize 100x images/wallet_icons/small/$f
-  convert -background none "images/wallet_icons/$f" -resize 25x images/wallet_icons/tiny/$f
-done
-convert "resSources/hacker.jpg" -resize 200x images/hacker.jpg
+mkdir images/wallet_icons/tiny/ images/wallet_icons/small/ 2> /dev/null
+
+resizeDeterministically() {
+  filename=$1
+  source=$2/$filename
+  target=$3/$filename
+  size=$4
+  tmp=/tmp/$filename
+  convert -background none $source -resize ${size}x $tmp \
+    && compare -metric AE $tmp $target NULL: || cp -f $tmp $target
+  rm $tmp
+}
+
+export -f resizeDeterministically
+
+files=$( ls images/wallet_icons/*.* )
+parallel resizeDeterministically {/} images/wallet_icons images/wallet_icons/small 100 ::: $files
+parallel resizeDeterministically {/} images/wallet_icons images/wallet_icons/tiny 25 ::: $files
+resizeDeterministically hacker.jpg resSources images 300
