178b07fed8d504f840d53c201cda3ca788dc434c
Leo Wandersleb <leo@leowandersleb.de>
2020-06-28 23:26:56 -0400

    fixed code duplication, fixed modal close

--- assets/js/walletlist.js
+++ assets/js/walletlist.js
@@ -6,3 +6,38 @@ $(document).ready(function(){
     $("#sidebarWalletListShowButton").hide()
   });
 });
+
+var lastId = ""
+
+function toggleApp(id) {
+  if ($('#details_' + id + ':visible').length) {
+    // hide
+    $('#modal').hide()
+    $('#card_' + id)
+      .css('left', '')
+      .css('top', '')
+      .css('righ', '')
+      .css('margin-left', '')
+      .css('margin-right', '')
+      .css('width', '')
+      .css('position', '')
+    $('#details_' + id).hide()
+    $('#show_' + id).show()
+    $('#hide_' + id).hide()
+  } else {
+    // show
+    $('#modal').show()
+    $('#card_' + id)
+      .css('top', $('#card_' + id).position().top)
+      .css('left', 0)
+      .css('right', 0)
+      .css('margin-left', 'auto')
+      .css('margin-right', 'auto')
+      .css('width', '25em')
+      .css('position', 'absolute')
+    $('#details_' + id).show()
+    $('#show_' + id).hide()
+    $('#hide_' + id).show()
+  }
+  lastId = id
+}
