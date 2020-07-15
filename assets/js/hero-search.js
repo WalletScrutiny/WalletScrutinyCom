a28d71826772bcb5d28cad48489b7f91b9c3b882
Leo Wandersleb <leo@leowandersleb.de>
2020-07-14 23:53:31 -0400

    improve search feature
    the wallet search now also searches the website url and appId of wallets
    the search results are sorted as in landingpage+moreApps.

--- assets/js/hero-search.js
+++ assets/js/hero-search.js
@@ -1,3 +1,14 @@
+var verdictOrder = "reproducible,nonverifiable,nosource,custodial,wip,nobtc,fewusers,defunct,nowallet"
+var sortedWallets = []
+window.addEventListener("load", function () { 
+  sortedWallets = Object.values(window.wallets).sort(function( a, b) {
+    if (a.verdict != b.verdict)
+      return verdictOrder.indexOf(a.verdict) - verdictOrder.indexOf(b.verdict)
+    if (a.users != b.users)
+      return Number(b.users) - Number(a.users)
+    return Number(b.ratings) - Number(a.ratings)
+  })
+})
 if (document.querySelectorAll(".hero-cta").length > 0) {
   var p = document.querySelectorAll(".hero-cta")[0]
   var t = document.createElement("div")
@@ -28,20 +39,19 @@ function searchCatalogue(t) {
   var filter = String("COIN").indexOf(v) !== -1 ? 4 : 2
   if (v.length > filter) {
     var f = 0
-    for (var key in window.wallets) {
-      var r = window.wallets[key]
-      var n = r.title
+    sortedWallets.forEach(function(r) {
+      var n = `${r.title} ${r.appId} ${r.website}`
       if(f<1){
         result.innerHTML = "<li><a style='font-size:.7rem;opacity:.7;text-style:italics;'>No matches</a></li>";
       }
       if (n.toUpperCase().indexOf(v) !== -1) {
         if(f==0){result.innerHTML = "";}
         var l = document.createElement("li");
-        l.innerHTML = `<a href='/posts/${key}'><img src='/images/wallet_icons/small/${r.icon}' class='results-list-wallet-icon' />${n}</a>`
+        l.innerHTML = `<a href='/posts/${r.appId}'><img src='/images/wallet_icons/small/${r.icon}' class='results-list-wallet-icon' />${r.title}</a>`
         result.append(l)
         f++
       }
-    }
+    })
   } else if(v.length!=0){
     var l = document.createElement("li")
     var rem = (filter+1)-v.length
