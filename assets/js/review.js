26ce4585e2e9418ff5f2436eb9fc4aba831e215e
vswee <github@sinaswee.com>
2021-01-06 23:16:45 +0000

    layout changes and time-elapsed script

---
+++ assets/js/review.js
@@ -0,0 +1,43 @@
+for(i=0; i<document.querySelectorAll(".calculate-time-elapsed").length; i++){
+  let t = document.querySelectorAll(".calculate-time-elapsed")[i];
+  t.innerHTML = elapsedTime(t.getAttribute("data"));
+}
+function elapsedTime(d) {
+  d = d.split(" ")[0];
+  d = new Date(d);
+  let e = new Date();
+  let n = e.getTime() - d.getTime();
+  n = n / 1000;
+
+  let year = n / (52 * 604800); 
+  n = (n % (52 * 604800)); 
+
+  let month = n / (4 * 604800); 
+  n = (n % (4 * 604800)); 
+
+  let week = (n / (604800)); 
+  n = (n % (604800)); 
+
+  let day = (n / (86400)); 
+  n = (n % (86400)); 
+  
+  let hour = n / 3600; 
+  n %= 3600; 
+  
+  let min = n / 60 ; 
+  n %= 60; 
+  
+  let sec = n; 
+
+  let options = { "years": year, "months": month, "weeks": week, "days": day, "hours": hour };
+
+  let max = "";
+  Object.keys(options).forEach(function (k) {
+    if (Number(options[k]) > 0 && String(max).length < 1) {
+        max +=  " (~" + Math.ceil(options[k]) + " " + k + " ago)";  
+      }
+  });
+  
+  return max;
+
+}
