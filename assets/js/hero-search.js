f792616805a7f9bc9f9fec9cfe5bfaee3d2187b7
vswee <github@sinaswee.com>
2021-02-19 17:31:38 +0000

    wip: change grouping layout for wallet search and reinstates full-with clickable

--- assets/js/hero-search.js
+++ assets/js/hero-search.js
@@ -35,7 +35,7 @@ function exitSearch() {
 }
 
 document.getElementById("exitSearchTrigger").addEventListener("mouseenter", function (event) { if (event.target != this) { return; } exitSearch() });
-document.getElementById("exitSearchTrigger").addEventListener("click", function ( event) { if (event.target != this) { return; } exitSearch() })
+document.getElementById("exitSearchTrigger").addEventListener("click", function (event) { if (event.target != this) { return; } exitSearch() })
 
 function focusResults(e) {
   e.preventDefault()
@@ -44,27 +44,7 @@ function focusResults(e) {
   }
 }
 
-function determineIconTag(e) {
-  if (!e) { return false; }
-  
-  var css;
-  switch (e) {
-    case 'app store':
-      css = 'app-store';
-      break;
-    case 'play store':
-      css = 'google-play';
-      break;
-    case 'fdroid catalogue':
-      css = 'f-droid';
-      break;
-      case 'windows':
-        css = 'windows';
-      break;
-    default:
-  }
-  return css;
-}
+
 
 
 function searchCatalogue(termInput) {
@@ -73,72 +53,59 @@ function searchCatalogue(termInput) {
   result.classList.add("results-list")
   const term = termInput.value.toUpperCase()
   const minTermLength = 1
-  const basePath = window.wallets.base_path
   if (term.length > minTermLength) {
     var matchCounter = 0
     window.orderedObs.forEach(function (wallet) {
       if (wallet.title) {
-      let searchableTerms = `${wallet.title} ${wallet.appId} ${wallet.website} ${wallet.developerWebsite} ${wallet.category} ${wallet.verdict}`
+        let searchableTerms = `${wallet.title} ${wallet.appId} ${wallet.website} ${wallet.developerWebsite} ${wallet.category} ${wallet.verdict}`
 
 
-      var more = wallet.versions && wallet.versions.length > 0 ? "<br><sub>(multiple versions)</sub>" : ""
-      var imgEx = '', lnkEx = '';
-      if (wallet.versions && wallet.versions.length > 0) {
-        for (i = 0; i < wallet.versions.length; i++) {
-          searchableTerms+=`${wallet.versions[i].category} ${wallet.versions[i].verdict} multi cross`;
-          var aURL = basePath + wallet.versions[i].url;
-          imgEx += `<a onclick="window.location.href = '${aURL}';"
-          href='${aURL}'><img
-          src='${basePath}/images/wallet_icons/${wallet.versions[i].folder}/small/${wallet.versions[i].icon}'
-          class='results-list-wallet-icon'/></a>`;
 
-          lnkEx += `<a onclick="window.location.href = '${aURL}';" href='${aURL}' class="${wallet.versions[i].verdict}">
-          <i class="fab fa-${determineIconTag(wallet.versions[i].category)} lnk-bdg-btn"></i>
-          <span>${wallet.versions[i].verdict}</span>
-        </a>`
-        }
-      }
-      if (matchCounter < 1) {
-        result.innerHTML = "<li><a style='font-size:.7rem;opacity:.7;text-style:italics;'>No matches</a></li>";
-      }
-      let index = searchableTerms.toLocaleUpperCase().indexOf(term);
-      // console.log(searchableTerms, searchableTerms.toLocaleUpperCase().substr(index-3, 3));
-      if (index !== -1) {
-        if (matchCounter == 0) {
-          result.innerHTML = ""
-        }
-        bi.classList.remove("fa-times")
-        bi.classList.add("fa-circle-notch")
-        const walletRow = document.createElement("li")
-        walletRow.style['animation-delay'] = matchCounter * .1 + 's'
-        walletRow.classList.add("actionable")
-        const analysisUrl = `${basePath}${wallet.url}`
-        walletRow.innerHTML = `<div>
-            <span class="icon-overlap">
-              <a onclick="window.location.href = '${analysisUrl}';"
-              href='${analysisUrl}'><img
-              src='${basePath}/images/wallet_icons/${wallet.folder}/small/${wallet.icon}'
-              class='results-list-wallet-icon'
-              /></a>
-              ${imgEx}
+        matchCounter < 1 && (result.innerHTML = "<li><a style='font-size:.7rem;opacity:.7;text-style:italics;'>No matches</a></li>")
+
+        let index = searchableTerms.toLocaleUpperCase().indexOf(term);
+
+        if (index !== -1) {
+          if (matchCounter == 0) {
+            result.innerHTML = ""
+          }
+
+          bi.classList.remove("fa-times")
+          bi.classList.add("fa-circle-notch")
+          const walletRow = document.createElement("li")
+          walletRow.style['animation-delay'] = matchCounter * .1 + 's'
+          walletRow.classList.add("actionable")
+          var compactedResults = '';
+          function cPlus(w) {
+            const basePath = window.wallets.base_path
+            var analysisUrl = `${basePath}${w.url}`
+            compactedResults += `<a class="result-pl-inner" onclick="window.location.href = '${analysisUrl}';"
+              href='${analysisUrl}'>
+              <img src='${basePath}/images/wallet_icons/${w.folder}/small/${w.icon}' class='results-list-wallet-icon' />
+            <span>${w.title}</span>
+            
+            <span class="badge-2 ${w.verdict}">
+                <i class="fab fa-${window.determineIconTag(w.category)}"></i>&nbsp;
+                <span>${w.verdict}</span>
             </span>
 
-            <span>${wallet.title}${more}</span>
-            
-            <div class="link-grid">
+            </a>`
+          }
+          cPlus(wallet)
+          var det = ""
+          if (wallet.versions && wallet.versions.length > 0) {
+            for (i = 0; i < wallet.versions.length; i++) {
+              searchableTerms += `${wallet.versions[i].category} ${wallet.versions[i].verdict} multi cross`;
+              cPlus(wallet.versions[i])
+            }
+            det = "-hom"
+          }
+          walletRow.innerHTML = `<div class="${det}">${compactedResults}</div>`;
 
-              <a onclick="window.location.href = '${analysisUrl}';" href='${analysisUrl}' class="${wallet.verdict}">
-                <i class="fab fa-${determineIconTag(wallet.category)} lnk-bdg-btn"></i>
-                <span>${wallet.verdict}</span>
-              </a>
-              ${lnkEx}
-            </div>
-
-            </div>`
-        result.append(walletRow)
-        matchCounter++
+          result.append(walletRow)
+          matchCounter++
+        }
       }
-    }
     })
   } else if (term.length != 0) {
     var l = document.createElement("li")
@@ -160,7 +127,6 @@ function searchCatalogue(termInput) {
 function heroUX(termInput) {
   termInput.focus()
   termInput.select()
-  // showInitialSuggestion()
   heroSearchScrollToTop(termInput)
 
   if (termInput.value.length > 0) {
@@ -178,47 +144,4 @@ function heroSearchScrollToTop(termInput) {
       behavior: 'smooth'
     })
   }
-}
-
-
-
-
-function showInitialSuggestion() {
-  var reproducibleKeysIcons = '';
-  // CAN BE MOVED OUTSIDE THIS FUNCTION TO PREVENT RE-BUILD
-  var k_ = [], base = window.wallets.base_path;
-  document.querySelectorAll(".results-list")[0].classList.add("quick-link");
-  if (reproducibleKeysIcons.length < 1) {
-    Object.keys(window.wallets).forEach(key => {
-      window.wallets[key].verdict && window.wallets[key].verdict == 'reproducible' && (k_.push(key));
-    })
-    var a = k_;
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    for (i = 0; i < k_.length; i++) {
-      var k = window.wallets[k_[i]],
-        p = k.category;
-      reproducibleKeysIcons += `<a onclick="window.location.href = '${k.url}';" href='${k.url}'><img src='${base}/images/wallet_icons/${p}/small/${k.icon}' class='results-list-wallet-icon' />
-      <span>
-      <span class="badge ${k.verdict}">${k.verdict}</span>
-      <i class="fab fa-${determineIconTag(k.category)}"></i>
-      <br>
-      ${k.title}</span>
-      </a>`
-    }
-  }
-  document.querySelectorAll(".results-list")[0].innerHTML = `<div class="-pl">${reproducibleKeysIcons}</div>`;
-  document.querySelectorAll(".results-list")[0].style.display = "";
-  document.querySelectorAll(".-pl")[0].scrollTo({left: 500})
-  
-  setTimeout(function () {
-    document.querySelectorAll(".-pl")[0].scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    })
-  }, 100);
- 
 }
