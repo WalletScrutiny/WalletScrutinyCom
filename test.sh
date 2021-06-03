13ab5208db60dcfd0c25df6f978b023045790cf9
Leo Wandersleb <leo@leowandersleb.de>
2021-06-03 18:58:05 -0400

    improve build comparison
    the apktool based unpacking of APKs misses some content as Emanuel noted
    in https://gitlab.com/walletscrutiny/walletScrutinyCom/-/issues/238
    This change uses zip instead.
    The Schildbach wallet actually had a diff in the relevant part. I
    bundled a fix provided by Andreas Schildbach in above issue, too.

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-downloadedApp="$1"
+downloadedApk="$1"
 # make sure path is absolute
-if ! [[ $downloadedApp =~ ^/.* ]]; then
-  downloadedApp="$PWD/$downloadedApp"
+if ! [[ $downloadedApk =~ ^/.* ]]; then
+  downloadedApk="$PWD/$downloadedApk"
 fi
 wsContainer="walletscrutiny/android:5"
 
@@ -46,34 +46,31 @@ usage() {
        test.sh - test if apk can be built from source
 
 SYNOPSIS
-       test.sh downloadedApp
+       test.sh downloadedApk
 
 DESCRIPTION
        This command tries to verify builds of apps that we verified before.
        
-       downloadedApp  The apk file we want to test.'
+       downloadedApk  The apk file we want to test.'
 }
 
-if [ ! -f "$downloadedApp" ]; then
+if [ ! -f "$downloadedApk" ]; then
   echo "APK file not found!"
   echo
   usage
   exit 1
 fi
 
-apkHash=$(sha256sum "$downloadedApp" | awk '{print $1;}')
+apkHash=$(sha256sum "$downloadedApk" | awk '{print $1;}')
 fromPlayFolder=/tmp/fromPlay$apkHash
 rm -rf $fromPlayFolder
-signer=$( getSigner "$downloadedApp" )
+signer=$( getSigner "$downloadedApk" )
 echo "Extracting APK content ..."
-containerApktool $fromPlayFolder "$downloadedApp" || exit 1
+containerApktool $fromPlayFolder "$downloadedApk" || exit 1
 appId=$( cat $fromPlayFolder/AndroidManifest.xml | head -n 1 | sed 's/.*package=\"//g' | sed 's/\".*//g' )
 versionName=$( cat $fromPlayFolder/apktool.yml | grep versionName | sed 's/.*\: //g' | sed "s/'//g" )
 versionCode=$( cat $fromPlayFolder/apktool.yml | grep versionCode | sed 's/.*\: //g' | sed "s/'//g" )
-fromPlayUnpacked=/tmp/fromPlay_"$appId"_"$versionCode"
 workDir="/tmp/test$appId"
-rm -rf $fromPlayUnpacked
-mv $fromPlayFolder $fromPlayUnpacked
 
 if [ -z $appId ]; then
   echo "appId could not be tetermined"
@@ -91,7 +88,7 @@ if [ -z $versionCode ]; then
 fi
 
 echo
-echo "Testing \"$downloadedApp\" ($appId version $versionName)"
+echo "Testing \"$downloadedApk\" ($appId version $versionName)"
 echo
 
 prepare() {
@@ -109,9 +106,11 @@ prepare() {
 
 result() {
   # collect results
-  fromBuildUnpacked="/tmp/fromBuild_${appId}_$versionCode"
-  rm -rf $fromBuildUnpacked
-  containerApktool $fromBuildUnpacked "$builtApk" || exit 1
+  fromPlayUnzipped="/tmp/fromPlay_${appId}_$versionCode"
+  fromBuildUnzipped="/tmp/fromBuild_${appId}_$versionCode"
+  rm -rf $fromBuildUnzipped $fromPlayUnzipped
+  unzip -d $fromPlayUnzipped "$downloadedApk" || exit 1
+  unzip -d $fromBuildUnzipped "$builtApk" || exit 1
   echo "Results:
 appId:          $appId
 signer:         $signer
@@ -120,16 +119,16 @@ apkVersionCode: $versionCode
 apkHash:        $apkHash
 
 Diff:
-$( diff --brief --recursive $fromPlayUnpacked $fromBuildUnpacked )
+$( diff --brief --recursive $fromPlayUnzipped $fromBuildUnzipped )
 
 Revision, tag (and its signature):
 $( git tag -v "$tag" )
 
 Run a full
-diff --recursive $fromPlayUnpacked $fromBuildUnpacked
-meld $fromPlayUnpacked $fromBuildUnpacked
+diff --recursive $fromPlayUnzipped $fromBuildUnzipped
+meld $fromPlayUnzipped $fromBuildUnzipped
 or
-diffoscope $downloadedApp $builtApk
+diffoscope \"$downloadedApk\" $builtApk
 for more details."
 }
 
