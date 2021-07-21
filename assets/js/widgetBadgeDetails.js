ebef7d09dae044d0bba8b45acb0591c4275adbd3
Leo Wandersleb <leo@leowandersleb.de>
2021-07-20 22:11:36 -0400

    fix social links in widgets and badges

--- assets/js/widgetBadgeDetails.js
+++ assets/js/widgetBadgeDetails.js
@@ -38,10 +38,10 @@ function getWidgetDetails(wallet) {
     ${ linkIf(wallet.shop,             "Official Store",    '<i class="fas fa-shopping-cart"></i>') }
     ${ linkIf(wallet.repository,       "Code Repository",   '<i class="fab fa-github"></i>') }
     ${ linkIf(wallet.issue,            "Issue",             '<i class="fa fa-bug" aria-hidden="true"></i>') }
-    ${ linkIf(wallet.providerTwitter,  "Provider Twitter",  '<i class="fab fa-twitter"></i>') }
-    ${ linkIf(wallet.providerFacebook, "Provider Facebook", '<i class="fab fa-facebook-f"></i>') }
-    ${ linkIf(wallet.providerReddit,   "Provider Reddit",   '<i class="fab fa-reddit"></i>') }
-    ${ linkIf(wallet.providerLinkedIn, "Provider LinkedIn", '<i class="fab fa-linkedin-in"></i>') }
+    ${ linkIf(wallet.providerTwitter && `https://twitter.com/${wallet.providerTwitter}`,                "Provider Twitter",  '<i class="fab fa-twitter"></i>') }
+    ${ linkIf(wallet.providerFacebook && `https://www.facebook.com/${wallet.providerFacebook}`,         "Provider Facebook", '<i class="fab fa-facebook-f"></i>') }
+    ${ linkIf(wallet.providerReddit && `https://www.reddit.com/r/${wallet.providerReddit}`,             "Provider Reddit",   '<i class="fab fa-reddit"></i>') }
+    ${ linkIf(wallet.providerLinkedIn && `https://www.linkedin.com/company/${wallet.providerLinkedIn}`, "Provider LinkedIn", '<i class="fab fa-linkedin-in"></i>') }
     </td></tr>
     </table><style>td{padding:.25rem .5rem .25rem 0;}tr{box-shadow: 0px 10px 2px -10px #ddd;}td > a{text-decoration: none;}</style>`
 }
