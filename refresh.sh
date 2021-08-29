b1012dec29eec89ba8e52fd18f4df08389c088b5
Leo Wandersleb <leo@leowandersleb.de>
2021-08-29 18:43:58 -0400

    add some stats to refresh
    With the amount of apps we are tracking now, the number of apps jumping
    to the next "downloads" level is probably proportional to surges in
    Bitcoin interest. Print it out with the refresh script

--- refresh.sh
+++ refresh.sh
@@ -59,4 +59,14 @@ for f in $( git diff -G'version' --name-only ); do
 done
 
 echo "Diff minus the boring stuff:"
-git diff --word-diff |  grep -v "latest\|ratings\|reviews\|---\|@\|index\|^diff\|Binary\|apkVersionName\|size\|updated\|^score:\|^rating\|^version\|^review\|^stars" | grep "+++\|"
+git diff --word-diff |  grep -v "latest\|ratings\|reviews\|---\|@\|index\|^diff\|Binary\|apkVersionName\|size\|updated\|^score:\|^rating\|^version\|^review\|^stars\|^users" | grep "+++\|"
+
+function moreSince {
+  when=$1
+  echo $( git diff @{$when} | grep '^-users: ' | wc -l )
+}
+
+echo "Apps that now have more users ..."
+echo "... than yesterday:  $( moreSince 'one.days.ago' )"
+echo "... than last week:  $( moreSince 'one.weeks.ago' )"
+echo "... than last month: $( moreSince 'one.months.ago' )"
