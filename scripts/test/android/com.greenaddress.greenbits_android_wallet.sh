51b6a85b5824f3514c40b1a7c312aa0e68a244ef
Leo Wandersleb <leo@leowandersleb.de>
2021-09-16 22:46:52 -0300

    green

--- scripts/test/android/com.greenaddress.greenbits_android_wallet.sh
+++ scripts/test/android/com.greenaddress.greenbits_android_wallet.sh
@@ -3,17 +3,17 @@
 test() {
   repo=https://github.com/Blockstream/green_android/
   tag=release_$versionName
-  builtApk=$workDir/app/green/build/outputs/apk/production/release/green-production-release-unsigned.apk
+  builtApk="$workDir/app/green/build/outputs/apk/production/release/BlockstreamGreen-v${versionName}-production-release-unsigned.apk"
   
   prepare
 
   # build
-  podman run -it --volume $PWD:/mnt --rm $wsContainer bash -x -c 'cd /mnt;
+  podman run -it --volume $PWD:/mnt --rm $wsContainer bash -x -c "cd /mnt;
       apt update;
-      apt install -y curl jq;
-      yes | /opt/android-sdk/tools/bin/sdkmanager "build-tools;29.0.2";
+      apt install -y curl jq openjdk-11-jdk;
+      yes | /opt/android-sdk/tools/bin/sdkmanager \"build-tools;29.0.2\";
       ./gradlew -x test clean assembleProductionRelease;
-      $takeUserActionCommand'
+      $takeUserActionCommand"
       
   result
 }
