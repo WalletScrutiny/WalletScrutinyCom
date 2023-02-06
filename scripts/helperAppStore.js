e74fd89bc16aa03e2dcd7bb95a1668ac6b157057
xrviv <dannyboygarcia@gmail.com>
2023-02-06 10:12:18 +0800

    Removing changes to helperAppStore.js

--- scripts/helperAppStore.js
+++ scripts/helperAppStore.js
@@ -1,9 +1,7 @@
 process.env.TZ = 'UTC' // fix timezone issues
 
 const apple = require('app-store-scraper')
-const fs = require('fs')
-// const fs = require('fs/promises')
-// Danny erased the commented line above, and it worked.
+const fs = require('fs/promises')
 const path = require('path')
 const helper = require('./helper.js')
 const { Semaphore } = require('async-mutex')
