82a0b0aa3ac0cdbc193128c02ddd9433be11494c
vswee <github@sinaswee.com>
2023-03-02 23:27:15 +0000

    fix menu

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -5,6 +5,7 @@
 $(document).ready(function(){
    // Sticky footer
   var bumpIt = function() {
+      $("body").setAttribute("_margin-bottom", $(".page__footer").outerHeight(true));
     },
     didResize = false;
 
