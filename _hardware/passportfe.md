3772700caf25b452017c8e852da0fd6fc1f09659
sethforprivacy <hello@sethforprivacy.com>
2023-06-01 15:38:02 -0400

    Update to latest Passport and move FE to new page

---
+++ _hardware/passportfe.md
@@ -0,0 +1,72 @@
+---
+title: Foundation Passport "Founder's Edition"
+appId: passportfe
+authors:
+- kiwilamb
+- leo
+released: 2020-07-01
+discontinued: 
+updated: 2022-08-02
+version: v1.1.0
+binaries: https://github.com/Foundation-Devices/passport-firmware/releases
+dimensions:
+- 38
+- 100
+- 23
+weight: 138
+provider: 
+providerWebsite: 
+website: https://foundationdevices.com/
+country: US
+price: 299USD
+repository: https://github.com/Foundation-Devices/passport-firmware
+issue: https://github.com/Foundation-Devices/passport-firmware/issues/40
+icon: passport.png
+bugbounty: https://foundationdevices.com/security/
+meta: outdated
+verdict: reproducible
+date: 2022-08-07
+signer: 
+reviewArchive:
+- date: 2021-12-01
+  version: 1.0.8-beta
+  appHash: 703feb6c387db47ea862ab55acfa984afa456c75dff22b21977459f68e7e1795
+  gitRevision: 8d95977073353d5addee069f2003f3974cd50595
+  verdict: reproducible
+- date: 2021-10-04
+  version: v1.0.7
+  appHash: 265716676ca91bd724ad48b28a6877841b216003b7b03bbfd6e5eee85a5c057a
+  gitRevision: ef2ffe05e70ed0485fa1526ea79a23bf80b15b4c
+  verdict: reproducible
+- date: 2021-08-17
+  version: v1.0.6
+  appHash: 606ca7ce1ba136988a36b445b2dae97508fbed062a8fdfa02f13ada69b6e92cd
+  gitRevision: 1cf92f351ed58ce13738940ad6e9fad35366dbd5
+  verdict: reproducible
+twitter: FOUNDATIONdvcs
+social:
+- https://www.linkedin.com/company/foundationdevices
+- https://t.me/foundationdevices
+- https://www.reddit.com/r/FoundationDevices/
+features: 
+
+---
+
+The provider released a new version. Here we try to reproduce it again, using
+our {% include testScript.html %}:
+
+```
+$ ./scripts/test/hardware/passport.sh 1.0.8
+...
+ad6b4f5f4ae0b7e05ec35415713ea1ff7dde3edf10870876b1c8bd07391419d1  build-Passport/firmware.bin
+
+root@cae95eed5f75:/passport-firmware/ports/stm32# exit
+ad6b4f5f4ae0b7e05ec35415713ea1ff7dde3edf10870876b1c8bd07391419d1  -
+ad6b4f5f4ae0b7e05ec35415713ea1ff7dde3edf10870876b1c8bd07391419d1  firmware-passport-v1.0.8.bin
+```
+
+As with other hardware wallets, we did not check if those clipped bytes are
+signatures or valid but leave that to actual code reviews: If the code
+of the hardware wallet does what is claimed, it interprets those bytes as
+signatures and checks them. In other words, if the public source code is secure,
+then the link with the binary is established. This product is **reproducible**.
--- scripts/test/hardware/passport.sh
+++ scripts/test/hardware/passport.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
-
-
-### provide this script with the version without "v" and the published buildHash
-
+# Provide this script with the version without "v"
+# You can also optionally provide the published build hash and SHA256 hash of the binary
+# to have the script compare hashes automatically.
 version=$1
 buildHash=$2
+releaseHash=$3
+fileName=v${version}-passport.bin
 
+# Remove any previous build artifacts
 rm -rf /tmp/passport/
-rm /tmp/passport-fw-${version}.bin
 
-cd /tmp
-wget https://github.com/Foundation-Devices/passport-firmware/releases/download/v${version}/passport-fw-${version}.bin
-sha256sum passport-fw-${version}.bin 
-mkdir passport
-cd passport
-podman run --rm -it --volume=$(pwd):/work/ ubuntu:18.04 bash -c "apt update; \
-    apt install --yes git python3-pip gcc-arm-none-eabi autotools-dev automake libusb-1.0-0-dev libtool; \
-    git clone https://github.com/Foundation-Devices/passport-firmware.git; \
-    cd passport-firmware; \
-    git checkout v${version}; \
-    make -C mpy-cross; \
-    cd ports/stm32/; \
-    make BOARD=Passport; \
-    sha256sum build-Passport/firmware.bin;echo $buildHash; \
-    mv build-Passport/firmware.bin /work/firmware-passport-v${version}.bin; \
-    bash"
-tail -c +2049 ../passport-fw-${version}.bin | sha256sum ; \
-    sha256sum firmware-passport-v${version}.bin; \
+# Prepare the directory for building Passport's firmware
+mkdir /tmp/passport
+cd /tmp/passport
+
+# Get the specified firmware release binary
+wget -q --show-progress https://github.com/Foundation-Devices/passport2/releases/download/v${version}/${fileName}
+
+# Clone the specified release branch
+git clone https://github.com/Foundation-Devices/passport2.git
+cd passport2
+git checkout v${version}
+
+# Install dependencies for building
+# Download and extract just to /tmp/passport/just
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /tmp/passport
+
+# Build and verify the sha256 hash of the specified firmware version
+/tmp/passport/just build-docker
+/tmp/passport/just build-firmware color
+/tmp/passport/just build-bootloader color
+
+# Print build hash and expected build hash
+echo "Built v${version} binary sha256 hash:"
+shasum -b -a 256 ports/stm32/build-Passport/firmware-COLOR.bin
+if [[ "$2" ]]; then
+    echo "Expected v${version} build hash:"
     echo $buildHash
+    echo "$buildHash *ports/stm32/build-Passport/firmware-COLOR.bin" | sha256sum --check
+fi
+# Print binary hash and expected binary hash
+echo "v${version} release binary sha256 hash:"
+shasum -b -a 256 ../${fileName}
+if [[ "$3" ]]; then
+    echo "Expected v${version} release binary hash:"
+    echo $releaseHash
+    echo "$releaseHash ../${fileName}" | sha256sum --check
+fi
