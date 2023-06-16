db8a804b315c456d9ead0abac66f5ad121246f03
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2023-06-16 22:11:01 +0000

    Make sed regex simpler

--- scripts/test/android/io.nunchuk.android.sh
+++ scripts/test/android/io.nunchuk.android.sh
@@ -10,7 +10,7 @@ test() {
       cd /mnt;
       printf '\nstoreFile=nunchuk.pfx\nstorePassword=nunchuk_alias\nkeyPassword=nunchuk_alias\nkeyAlias=nunchuk_alias' > keystore.properties;
       keytool -genkey -alias nunchuk_alias -keystore nunchuk-app/nunchuk.pfx -storetype PKCS12 -keyalg RSA -keysize 4096 -storepass nunchuk_alias -keypass nunchuk_alias -validity 10000 -dname CN=IL;
-      sed -i 's/.*nativeSdk: \"io\.nunchuk\.android:nativesdk.*/nativeSdk: \"com.github.nunchuk-io:nunchuk-android-nativesdk-prebuild:1.1.60@aar\"/' configs/dependencies.gradle;
+      sed -i 's/io.nunchuk.android:nativesdk:\${nativeSdkVersion}/com.github.nunchuk-io:nunchuk-android-nativesdk-prebuild:1.1.60@aar/g' configs/dependencies.gradle;
       ./gradlew assembleRelease;
       $takeUserActionCommand"
 }
