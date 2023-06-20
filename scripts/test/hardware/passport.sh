27bbd7c648bf9ad214e6bbdc85c6a013d3a4a402
Leo Wandersleb <leo@leowandersleb.de>
2023-06-19 21:49:38 -0400

    passport script refinements

--- scripts/test/hardware/passport.sh
+++ scripts/test/hardware/passport.sh
@@ -16,6 +16,7 @@ else
     echo 'Please specify one of "color" or "mono"'
     exit 1
 fi
+model=$2
 
 # Remove any previous build artifacts
 rm -rf /tmp/passport/
@@ -36,51 +37,33 @@ git checkout v${version}
 docker build -t ${dockerImage} .
 
 # Build mpy-cross within the Docker image
-docker run --rm -v "$PWD":/workspace \
-    -u $(id -u):$(id -g) \
-    -v $(pwd):/workspace \
-    -w /workspace \
-    -e MPY_CROSS="/workspace/mpy-cross/mpy-cross-docker" \
+docker run --rm \
+    --volume "$PWD":/workspace \
+    --user $(id -u):$(id -g) \
+    --workdir /workspace \
+    --env MPY_CROSS="/workspace/mpy-cross/mpy-cross-docker" \
     ${dockerImage} \
     make -C mpy-cross PROG=mpy-cross-docker BUILD=build-docker
-if [[ $2 == "color" ]]; then
-    docker run --rm -v "$PWD":/workspace \
-        -u $(id -u):$(id -g) \
-        -v $(pwd):/workspace \
-        -w /workspace \
-        -e MPY_CROSS="/workspace/mpy-cross/mpy-cross-docker" \
-        ${dockerImage} \
-        /usr/local/bin/just ports/stm32/build color
-elif [[ $2 == "mono" ]]; then
-    docker run --rm -v "$PWD":/workspace \
-        -u $(id -u):$(id -g) \
-        -v $(pwd):/workspace \
-        -w /workspace \
-        -e MPY_CROSS="/workspace/mpy-cross/mpy-cross-docker" \
-        ${dockerImage} \
-        /usr/local/bin/just ports/stm32/build mono
-fi
+docker run --rm \
+    --volume "$PWD":/workspace \
+    --user $(id -u):$(id -g) \
+    --workdir /workspace \
+    --env MPY_CROSS="/workspace/mpy-cross/mpy-cross-docker" \
+    ${dockerImage} \
+    /usr/local/bin/just ports/stm32/build ${model}
 
 # Print build hash and expected build hash
 echo "Built v${version} binary sha256 hash:"
-if [[ $2 == "color" ]]; then
-    shasum -b -a 256 ports/stm32/build-Passport/firmware-COLOR.bin
-elif [[ $2 == "mono" ]]; then
-    shasum -b -a 256 ports/stm32/build-Passport/firmware-MONO.bin
-fi
+sha256sum ports/stm32/build-Passport/firmware-${model^^}.bin
 if [[ "$3" ]]; then
     echo "Expected v${version} build hash:"
     echo $buildHash
-    if [[ $2 == "color" ]]; then
-        echo "$buildHash *ports/stm32/build-Passport/firmware-COLOR.bin" | sha256sum --check
-    elif [[ $2 == "mono" ]]; then
-        echo "$buildHash *ports/stm32/build-Passport/firmware-MONO.bin" | sha256sum --check
-    fi
+    echo "$buildHash *ports/stm32/build-Passport/firmware-${model^^}.bin" | sha256sum --check
 fi
 
 # Print binary hash and expected binary hash
 echo "v${version} release binary sha256 hash:"
-shasum -b -a 256 ../${fileName}
+sha256sum ../${fileName}
 if [[ "$4" ]]; then
     echo "Expected v${version} release binary hash:"
     echo $releaseHash
@@ -91,11 +74,7 @@ fi
 echo "Comparing v${version} stripped release binary hash:"
 echo "For more info, visit https://github.com/Foundation-Devices/passport2/blob/main/REPRODUCIBILITY.md#verify-release-binary-hash"
 dd if=../${fileName} of=no-header-${fileName} ibs=2048 skip=1 status=none
-if [[ $2 == "color" ]]; then
-    binaryHash=($(shasum -b -a 256 ports/stm32/build-Passport/firmware-COLOR.bin))
-elif [[ $2 == "mono" ]]; then
-    binaryHash=($(shasum -b -a 256 ports/stm32/build-Passport/firmware-MONO.bin))
-fi
+binaryHash=($(sha256sum ports/stm32/build-Passport/firmware-${model^^}.bin))
 echo "Expected v${version} build hash:"
 echo $binaryHash
 echo "$binaryHash no-header-${fileName}" | sha256sum --check
