05c57e1b4c8e681c765573d6e1a98664aec66ec0
Leo Wandersleb <leo@leowandersleb.de>
2023-06-29 02:13:54 -0400

    move all non-ok meta verdicts and verdict "wip" out of view
    On the landing page, show the following verdict groups only in the chart and remove them from the dropdown:
    * not relevant: many products nobody wants to go through for 340 pages
    * wip: kind of another "not relevant"
    * "all": 485 pages
    
    add meta "outdated" to the wip verdictGroup
    add all other non-ok metas to "not relevant"
    cleanup code

--- _plugins/replace-by-regex.rb
+++ _plugins/replace-by-regex.rb
@@ -2,8 +2,6 @@ module Jekyll
   module RegexFilter
     def replace_regex(input, reg_str, repl_str)
       re = Regexp.new reg_str
-
-      # This will be returned
       input.gsub re, repl_str
     end
   end
