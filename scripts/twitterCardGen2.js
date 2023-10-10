16746b0a3f749b2d6d4c1ee33491d7d16fdba43f
danny garcia <dannyboygarcia@gmail.com>
2023-10-10 19:46:06 +0800

    Verdict Overhaul

--- scripts/twitterCardGen2.js
+++ scripts/twitterCardGen2.js
@@ -30,7 +30,90 @@ directories.forEach((directory) => {
                 const version = versionMatch ? versionMatch[1].replace('.', ',') : 'N/A'
 
                 const verdictMatch = data.match(/verdict: (.+)/)
-                const verdict = verdictMatch ? verdictMatch[1].replace('.', ',').toUpperCase() : 'N/A'
+                const verdict = verdictMatch ? verdictMatch[1].replace('.', ',') : 'N/A'
+
+                let verdictDescription = `-pointsize 15 -fill black -draw "text 230,215 `;
+
+                // Use an if statement to generate different output based on "verdict"
+                if (verdict === 'custodial') {
+                    const drawOption = `'Custodial: The provider holds the keys'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'diy') {
+                    const drawOption = `'DIY: Do-It-Yourself Project'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'fake') {
+                    const drawOption = `'Fake: The product mimics a popular competitor!'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'fewusers') {
+                    const drawOption = `'Few users: This product has too few users for now to be reviewed in detail."`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'ftbfs') {
+                    const drawOption = `'FTBFS: Failed to build from source provided!'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'nobinary') {
+                    const drawOption = `'No Binary: This product comes without a binary.'"`;
+                    verdictDescription += `${drawOption}`;
+                }  
+                else if (verdict === 'nobtc') {
+                    const drawOption = `'No BTC: This wallet does not support Bitcoin.'"`;
+                    verdictDescription += `${drawOption}`;
+                }  
+                else if (verdict === 'noita') {
+                    const drawOption = `'Bad Interface: The device interface does not let you verify what is being signed'"`;
+                    verdictDescription += `${drawOption}`;
+                }  
+                else if (verdict === 'nonverifiable') {
+                    const drawOption = `'Not reproducible from source provided'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'nosendreceive') {
+                    const drawOption = `'No sending or receiving bitcoins'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'nosource') {
+                    const drawOption = `'No source for current release found'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'nowallet') {
+                    const drawOption = `'Not an actual wallet'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'obfuscated') {
+                    const drawOption = `'Obfuscated: The binary contains active obfuscation'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'obsolete') {
+                    const drawOption = `'Obsolete: Not updated in a long time'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'outdated') {
+                    const drawOption = `'Outdated: Review might be outdated'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'plainkey') {
+                    const drawOption = `'Leaks Keys: This device requires sharing private key material!'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'prefilled') {
+                    const drawOption = `'Prefilled: The device is delivered with the private keys as defined by the provider!'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'reproducible') {
+                    const drawOption = `'Reproducible when tested'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'sealed-noita') {
+                    const drawOption = `'Transactions are signed blindly'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'sealed-plainkey') {
+                    const drawOption = `'Sealed Plainkey: Keys are sealed at first, but may be shared when spending'"`;
+                    verdictDescription += `${drawOption}`;
+                } else if (verdict === 'unreleased') {
+                    const drawOption = `'Un-Released!'"`;
+                    verdictDescription += `${drawOption}`;
+                }  else if (verdict === 'vapor') {
+                    const drawOption = `'Vaporware!'"`;
+                    verdictDescription += `${drawOption}`;
+                }  else if (verdict === 'wip') {
+                    const drawOption = `'Review is Work in Progress'"`;
+                    verdictDescription += `${drawOption}`;
+                } else {
+                    const drawOption = `'Unknown verdict'"`;
+                    verdictDescription += `${drawOption}`;
+                }
+
+                
+
 
                 const releasedMatch = data.match(/released: (.+)/)
                 const released = releasedMatch ? releasedMatch[1].replace('.', ',') : 'N/A'
@@ -45,23 +128,24 @@ directories.forEach((directory) => {
                 const developerName = developerNameMatch ? developerNameMatch[1].replace('.', ',') : 'N/A'
 
                 const usersMatch = data.match(/users: (.+)/)
-                const users = usersMatch ? usersMatch[1].replace('.', ',') : 'N/A'
-
+                const users = usersMatch ? usersMatch[1].replace('. ', ',') : 'N/A'
+                
                 try {
                 const iconPath = `${currentImageFolder}/${wsID}.png`
                 execSync(`convert ${twcardFolder}/twitterImageBGBig.png -font ./assets/fonts/Barlow/barlow-v12-latin-500.ttf \
                     -fill black \
-                    -pointsize 26 -draw "text 230,170 \\"${title}\\"" \
-                    -pointsize 18 -fill gray -draw "text 230,190 \\"version: ${version}\\"" \
-                    -pointsize 18 -fill black -draw "text 230,210 \\"${verdict}\\"" \
+                    -pointsize 26 -kerning 0 -draw "text 230,170 \\"${title}\\"" \
+                    -pointsize 16 -kerning -1 -fill gray -draw "text 230,190 \\"version: ${version}\\"" \
+                    ${verdictDescription}\
                     -draw "stroke gray stroke-width 1 line 230,225 670,225" \
-                    -pointsize 18 -fill black -draw "text 230,250 'Developer:'" -fill gray -draw "text 330,250 '${developerName}'" \
-                    -pointsize 18 -fill black -draw "text 230,275 'Downloads:'" -fill gray -draw "text 330,275 '${users}'" \
-                    -pointsize 18 -fill black -draw "text 230,300 'Released on:'" -fill gray -draw "text 330,300 '${released}'" \
-                    -pointsize 18 -fill black -draw "text 230,325 'Updated on:'" -fill gray -draw "text 330,325 '${updated}'" \
-                    -pointsize 18 -fill black -draw "text 230,350 'Latest review:'" -fill gray -draw "text 330,350 '${date}'" \
+                    -pointsize 18 -fill black -draw "text 230,250 'Developer:'" -fill gray -draw "text 350,250 '${developerName}'" \
+                    -pointsize 18 -fill black -draw "text 230,275 'Downloads:'" -fill gray -draw "text 350,275 '${users}'" \
+                    -pointsize 18 -fill black -draw "text 230,300 'Released on:'" -fill gray -draw "text 350,300 '${released}'" \
+                    -pointsize 18 -fill black -draw "text 230,325 'Updated on:'" -fill gray -draw "text 350,325 '${updated}'" \
+                    -pointsize 18 -fill black -draw "text 230,350 'Latest review:'" -fill gray -draw "text 350,350 '${date}'" \
                     ${iconPath} -geometry +100+150 -composite \
                     ${socialOutput}/${directory}/${file}`)
+
                 totalFilesProcessed++
             } catch (error) {
                 console.error('Error processing file:', file, "Error message:", error.message)
