88a0763edb376a0c93f1df08ddb67b9189110f22
dannygarcia <dannyboygarcia@gmail.com>
2023-10-12 23:19:57 +0800

    so far so good - minus foreign characters

--- scripts/twitterCardGen3.js
+++ scripts/twitterCardGen3.js
@@ -13,7 +13,12 @@ const mdFolders = ['_android', '_bearer', '_hardware', '_iphone']; // MD files f
 const basePath = path.join(__dirname, '..');
 const backgroundImage = path.join(basePath, 'images', 'twCard', 'twitterImageBG600x600.jpg');
 const iconsBasePath = path.join(basePath, 'images', 'wIcons'); // Icons base path 
-const barlowFontPath = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow.fnt');
+const barlow30path = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow30black', '7R9s0RBr248p1NXogRG5taD4.ttf.fnt');
+const barlow27path = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow27black', 'wLGmfyMfmpMdLRpomfzsFBfq.ttf.fnt');
+const barlow25path = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow25black', 'jMYUacOdgyFacfesoWuVlXmp.ttf.fnt');
+const barlow25graypath = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow25gray', 'dWxVsEOMx8Dxe4UUXvy0leGe.ttf.fnt');
+const barlow23path = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow23black', '2KWA9EgA5hCXd2SVQJcsZf3Q.ttf.fnt');
+const barlow18path = path.join(basePath, 'assets', 'fonts', 'Barlow', 'barlow-fnt', 'barlow18black', 'zgfCog9D9HWvpLNyuFjDdXss.ttf.fnt');
 
 const verdictMap = {
     'custodial': 'Custodial: The provider holds the keys',
@@ -51,7 +56,12 @@ async function processFiles() {
     const startTime = Date.now();
 
     // Load font within the function
-    const barlowFont = await Jimp.loadFont(barlowFontPath);
+    const barlow30 = await Jimp.loadFont(barlow30path);
+    const barlow27 = await Jimp.loadFont(barlow27path);
+    const barlow25 = await Jimp.loadFont(barlow25path);
+    const barlow25g = await Jimp.loadFont(barlow25graypath);
+    const barlow23 = await Jimp.loadFont(barlow23path);
+    const barlow18 = await Jimp.loadFont(barlow18path);
 
     for (let mdFolder of mdFolders) {
         const mdFilesPath = path.join(basePath, mdFolder); // MD files path
@@ -90,7 +100,7 @@ async function processFiles() {
                     };
                  
                     // calculate the number of lines the title will occupy
-                    const titleLineCount = Math.ceil(Jimp.measureText(barlowFont, data.title) / 350); // assuming maxWidth is 250 as in your code
+                    const titleLineCount = Math.ceil(Jimp.measureText(barlow25, data.title) / 350); // assuming maxWidth is 250 as in your code
 
                     const iconImage = path.join(basePath, 'images', 'wIcons', mdFolder.substring(1), 'small', `${data.icon}`);
                     const tempImagePath = path.join(basePath, 'tempImage.png');
@@ -103,39 +113,76 @@ async function processFiles() {
                     // Load the temporary image for text addition
                     const image = await Jimp.read(tempImagePath);
 
-                    const wrappedTitle = wrapText(data.title, 27); // adjust the length as needed
+                    const wrappedTitle = wrapText(data.title, 32); // adjust the length as needed
                     for (let i = 0; i < wrappedTitle.length; i++) {
-                        image.print(barlowFont, 150, 130 + (i * 40), wrappedTitle[i]);
+                        image.print(barlow30, 150, 130 + (i * 40), wrappedTitle[i]);
                     }
                     y += wrappedTitle.length * 40;  // adjust the multiplier as per the font size
 
                     // Writing version with Barlow font
-                    image.print(barlowFont, 150, 165, data.version);
+                    image.print(barlow18, 55, 235, data.version);
                     y += 30; // Increment Y by an estimated height for the next item
 
                     // Writing verdict with Barlow font
-                    const wrappedVerdict = verdictMap[data.verdict] || data.verdict; // Fallback to data.verdict if not found in map
-                    image.print(barlowFont, 150, 205, { text: data.verdict, alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT, maxWidth: 500 });
-                    y += 60;  // Increment Y by an estimated height for the wrapped verdict
+                    const mappedVerdict = verdictMap[data.verdict] || data.verdict;
+                    const wrappedVerdict = wrapText(mappedVerdict, 40); 
+                    for (let i = 0; i < wrappedVerdict.length; i++) {
+                        image.print(barlow25, 150, 225 + (i * 30), wrappedVerdict[i]);
+                    }
+                    y += wrappedVerdict.length * 23; // adjust the multiplier as per the font size
+
+                    // Separator line
+
+                    // Drawing a thin gray horizontal line
+                    const lineStartX = 175;    // Start of the line on X-axis
+                    const lineStartY = 317;    // Start of the line on Y-axis
+                    const lineLength = 300;    // Length of the line
+                    const lineHeight = 1;      // Height of the line
+                    const grayColor = Jimp.rgbaToInt(128, 128, 128, 255); // RGBA value for gray
+
+                    image.scan(lineStartX, lineStartY, lineLength, lineHeight, (x, y, idx) => {
+                        image.bitmap.data[idx + 0] = (grayColor >> 24) & 0xFF; // Red channel
+                        image.bitmap.data[idx + 1] = (grayColor >> 16) & 0xFF; // Green channel
+                        image.bitmap.data[idx + 2] = (grayColor >> 8) & 0xFF;  // Blue channel
+                        image.bitmap.data[idx + 3] = grayColor & 0xFF;         // Alpha channel
+                    });
+
+                    y += lineHeight + 10; 
+                    // Labels        
+                    image.print(barlow25g, 150, 337, "Developer:");
+                    y += 30;
+
+                    image.print(barlow25g, 150, 372, "Downloads:");
+                    y += 30;
+
+                    image.print(barlow25g, 150, 407, "Released:");
+                    y += 30;
+
+                    image.print(barlow25g, 150, 442, "Latest Release:");
+                    y += 30;
+
+                    image.print(barlow25g, 150, 477, "Last Analyzed:");
+                    y += 30;
 
                     // Writing developerName with Barlow font
-                    image.print(barlowFont, 150, 250, { text: data.developerName, alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT, maxWidth: 500 });
-                    y += 60;  // Increment Y by an estimated height for the wrapped developer name
+                    const clippedDeveloperName = data.developerName.length > 15 ? data.developerName.substr(0, 15) + "..." : data.developerName;
+                    image.print(barlow25, 340, 337, clippedDeveloperName);
+                    y += 30; // Adjust y for the next item, depending on the font size you want.
 
                     // Writing users with Barlow font
-                    image.print(barlowFont, 150, 295, `Users: ${data.users}`);
+                    image.print(barlow25, 340, 372, `${data.users}`);
                     y += 30;
 
                     // Writing released date with Barlow font
-                    image.print(barlowFont, 150, 335, `Released: ${data.released}`);
+                    image.print(barlow25, 340, 407, `${data.released}`);
                     y += 30;
 
                     // Writing updated date with Barlow font
-                    image.print(barlowFont, 150, 375, `Updated: ${data.updated}`);
+                    image.print(barlow25, 340, 442, `${data.updated}`);
                     y += 30;
 
                     // Writing date with Barlow font
-                    image.print(barlowFont, 150, 415, `Date: ${data.date}`);
+                    image.print(barlow25, 340, 477, `${data.date}`);
                     y += 30;
 
                     const outputPath = `${basePath}/images/social/${mdFolder.substring(1)}/${file.replace('.md', '.png')}`;
