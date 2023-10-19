e5cfe60036851eec3875834be4217f43c61441ea
dannygarcia <dannyboygarcia@gmail.com>
2023-10-19 20:57:38 +0800

    Refactor 1

--- scripts/twitterCardGen.mjs
+++ scripts/twitterCardGen.mjs
@@ -1,45 +1,33 @@
+// Import libraries
 import fs from 'fs';
 import { createCanvas, loadImage, registerFont } from 'canvas';
-import util from 'util';
 import yaml from 'js-yaml';
-import child_process from 'child_process';
 import path from 'path';
 import pLimit from 'p-limit';
 
+// Constants
 const fsp = fs.promises;
-const limit = pLimit(8); // Allow n concurrent async operations
+const limit = pLimit(8); // Allow 8 concurrent async operations
+const basePath = '.';
+const mdFolders = ['_android', '_bearer', '_hardware', '_iphone']; // MD file folders
+const backgroundImage = path.join(basePath, 'images', 'twCard', 'twitterImageBG800x600.jpg');
+const iconsBasePath = path.join(basePath, 'images', 'wIcons');
+const fallbackIcon = path.join(basePath, 'images', 'smallNoicon.png');
+const verdictMap = loadVerdicts('_data/verdicts');
 
-const { promisify } = util;
-const exec = promisify(child_process.exec);
+// Timer variables
+let totalFiles = 0;
+let totalTime = 1000;
+let oldTotalFiles = 0;
+const startTime = Date.now();
 
+// Utility Functions - Text Wrapping
 function wrapText(text, length) {
     const regex = new RegExp(`(?:(?:\\S{${length}}|.{1,${length}})(?:\\s|$))`, 'g');
     return text.match(regex) || [];
 }
 
-const mdFolders = ['_android', '_bearer', '_hardware', '_iphone']; // MD files folders
-const basePath = '.'; // Base path to directory /
-const backgroundImage = path.join(basePath, 'images', 'twCard', 'twitterImageBG800x600.jpg'); // Loads a 800x600 jpg image as a background
-const iconsBasePath = path.join(basePath, 'images', 'wIcons'); // Icons base path 
-const fallbackIcon = path.join(basePath, 'images', 'smallNoicon.png'); 
-
-const verdictMap = {};
-const verdictPath = '_data/verdicts';
-fs.readdirSync(verdictPath).forEach((filename) => {
-  if (filename.endsWith('.yml')) {
-    const filePath = path.join(verdictPath, filename);
-    const verdict = path.parse(filename).name;
-    const yamlData = fs.readFileSync(filePath, 'utf8');
-    const data = yaml.load(yamlData);
-    verdictMap[verdict] = data.title;
-  }
-});
-
-// Width and Heights variables for canvas
-const width = 800;
-const height = 600;
-const startTime = Date.now();
-
+// Utility Functions - Date Formatter
 function formatDate(dateString) {
     const date = new Date(dateString);
     const year = date.getFullYear();
@@ -48,10 +36,22 @@ function formatDate(dateString) {
     return `${year}-${month}-${day}`;
 }
 
-let totalFiles = 0;
-let totalTime = 1000;
-let oldTotalFiles = 0;
+// Utility Functions - Verdict Mapper
+function loadVerdicts(verdictPath) {
+    const verdictMap = {};
+    fs.readdirSync(verdictPath).forEach((filename) => {
+      if (filename.endsWith('.yml')) {
+        const filePath = path.join(verdictPath, filename);
+        const verdict = path.parse(filename).name;
+        const yamlData = fs.readFileSync(filePath, 'utf8');
+        const data = yaml.load(yamlData);
+        verdictMap[verdict] = data.title;
+      }
+    });
+    return verdictMap;
+  }
 
+// Progress Tracking Function
 async function showProgress() {
     const i = setInterval(() => {
         const oldTotalTime = totalTime;
@@ -73,28 +73,12 @@ async function processFilesTimed() {
   await processFiles();
 }
 
-async function processFiles() {
-    const socialImagesFolderPath = `${basePath}/images/social`;
-    fs.existsSync(socialImagesFolderPath) || fs.mkdirSync(socialImagesFolderPath);
-
-    const asyncTasks = [];
-    for (let mdFolder of mdFolders) {
-        const mdFilesPath = path.join(basePath, mdFolder); // MD files path
-        const platform = mdFolder.substring(1);
-        const iconsPath = path.join(iconsBasePath, platform, 'small'); // Icons path
-        const files = fs.readdirSync(mdFilesPath);
-        const outputFolderPath = `${basePath}/images/social/${platform}`;
-        fs.existsSync(outputFolderPath) || fs.mkdirSync(outputFolderPath);
-
-        for (let file of files) {
-            totalFiles++;
-            asyncTasks.push(limit(() => processOneFile(platform, mdFilesPath, file, outputFolderPath)));
-        }
-    }
-    await Promise.all(asyncTasks);
-}
+// Core Functions - Canvas Image and Text Overlays
 
 async function drawOnCanvas(data, bgImage, iconImage) {
+    // Width and Heights variables for canvas
+    const width = 800;
+    const height = 600;
     const canvas = createCanvas(width, height);
     const ctx = canvas.getContext('2d');
     registerFont('assets/fonts/Barlow/barlow-v12-latin-500.ttf', { family: 'Barlow' });
@@ -117,34 +101,49 @@ async function drawOnCanvas(data, bgImage, iconImage) {
         ctx.font = '36px Barlow';
         ctx.fillText(currentLine, 225, 130 + (i * 40));
     }
+    // Version
     if (data.version) {
         ctx.font = '18px Barlow';
         ctx.fillStyle = 'gray';
-        ctx.fillText('Version: ', 50, 300); 
-        // Version No:
-        ctx.font = '18px Barlow';
-        ctx.fillStyle = 'black';
-        ctx.fillText(data.version, 125, 300);
+        ctx.fillText('Version:', 50, 300);
+        // Wrapped version
+        const wrappedVersion = wrapText(data.version, 7);  // adjust the length as needed
+    
+        for (let i = 0; i < wrappedVersion.length; i++) {
+            const currentLine = wrappedVersion[i];
+            // Render the text with the selected font, size, and color
+            ctx.font = '18px Barlow';
+            ctx.fillStyle = 'black';
+            ctx.fillText(currentLine, 125, 300 + (i * 20));  // Adjust vertical spacing as needed
+        }
     }
-
+    
     // Verdict 
     const mappedVerdict = verdictMap[data.verdict] || data.verdict || 'Unknown Verdict';
-    const wrappedVerdict = wrapText(mappedVerdict, 37);
+    const wrappedVerdict = wrapText(mappedVerdict, 41);
     ctx.font = '30px Barlow';  
     for (let i = 0; i < wrappedVerdict.length; i++) {
         const currentLine = wrappedVerdict[i];
         ctx.fillText(currentLine, 225, 225 + (i * 30));
     }
     
+    // Developer Name
     if (data.developerName) {
         ctx.font = '24px Barlow';
         ctx.fillStyle = 'gray';
         ctx.fillText('Developer:', 225, 300);
-        // Developer Name:
-        ctx.fillStyle = 'black';
-        ctx.fillText(data.developerName, 355, 300);
+        
+        // Wrap Developer Name
+        const wrappedDeveloperName = wrapText(data.developerName, 37);  // adjust the length as needed
+    
+        for (let i = 0; i < wrappedDeveloperName.length; i++) {
+            const currentLine = wrappedDeveloperName[i];
+            // Render the text with the selected font, size, and color
+            ctx.fillStyle = 'black';
+            ctx.fillText(currentLine, 355, 300 + (i * 30));  // Adjust vertical spacing as needed
+        }
     }
-
+    
     // Separator line code
     //------------------------------
     ctx.globalAlpha = 0.3; // 70% transparency
@@ -198,6 +197,7 @@ async function drawOnCanvas(data, bgImage, iconImage) {
     return canvas;
 }
 
+// Core Functions - Process One File
 async function processOneFile(platform, mdFilesPath, file, outputFolderPath) {
     try {
         const parts = fs.readFileSync(path.join(mdFilesPath, file), 'utf-8').split('---\n');
@@ -230,4 +230,27 @@ async function processOneFile(platform, mdFilesPath, file, outputFolderPath) {
         console.error(`Error processing file ${file}: `, error);
     }
 }
+
+// Core Functions - Process Files
+async function processFiles() {
+    const socialImagesFolderPath = `${basePath}/images/social`;
+    fs.existsSync(socialImagesFolderPath) || fs.mkdirSync(socialImagesFolderPath);
+
+    const asyncTasks = [];
+    for (let mdFolder of mdFolders) {
+        const mdFilesPath = path.join(basePath, mdFolder); // MD files path
+        const platform = mdFolder.substring(1);
+        const iconsPath = path.join(iconsBasePath, platform, 'small'); // Icons path
+        const files = fs.readdirSync(mdFilesPath);
+        const outputFolderPath = `${basePath}/images/social/${platform}`;
+        fs.existsSync(outputFolderPath) || fs.mkdirSync(outputFolderPath);
+
+        for (let file of files) {
+            totalFiles++;
+            asyncTasks.push(limit(() => processOneFile(platform, mdFilesPath, file, outputFolderPath)));
+        }
+    }
+    await Promise.all(asyncTasks);
+}
+
 processFilesTimed();
