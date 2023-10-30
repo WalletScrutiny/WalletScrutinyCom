0ceafd02e1983857074deb8a333ebcd6b500af2e
Leo Wandersleb <leo@leowandersleb.de>
2023-10-30 17:25:32 -0300

    Remove dependencies, update vulnerable package
    The following packages were removed:
    * browser-sync
    * core-js
    * eslint
    * gulp-load-plugins
    * jimp
    * npm-run-all
    * onchange
    * sleep
    * sync-request
    * @typescript-eslint/eslint-plugin
    * @typescript-eslint/parser
    * uglify-js
    * yaml-ast-parser
    All remaining dependencies were moved to devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require('gulp')
-var shell = require('gulp-shell')
-var sass = require('gulp-sass')(require('node-sass'))
-var minify = require('gulp-minify')
-var rename = require('gulp-rename')
-sass.compiler = require('node-sass')
-var del = require('del')
-const htmlmin = require('gulp-htmlmin')
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var sass = require('gulp-sass')(require('node-sass'));
+var minify = require('gulp-minify');
+var rename = require('gulp-rename');
+sass.compiler = require('node-sass');
+var del = require('del');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('minify', () => {
   return gulp
@@ -16,8 +16,8 @@ gulp.task('minify', () => {
         removeComments: true
       })
     )
-    .pipe(gulp.dest('_site'))
-})
+    .pipe(gulp.dest('_site'));
+});
 
 gulp.task('minjs', () => {
   return gulp.src('_site/**/*.js')
@@ -27,39 +27,39 @@ gulp.task('minjs', () => {
       },
       ignoreFiles: ['.min.js', '-min.js']
     }))
-    .pipe(gulp.dest('_site/'))
-})
+    .pipe(gulp.dest('_site/'));
+});
 
 gulp.task('cleanjs', () => {
-  return del('_site/**/*.js')
-})
+  return del('_site/**/*.js');
+});
 
 gulp.task('rename', () => {
   return gulp
     .src('./_site/**/*.jsm')
     .pipe(rename({ extname: '.js' }))
-    .pipe(gulp.dest('./_site/'))
-})
+    .pipe(gulp.dest('./_site/'));
+});
 
 gulp.task('cleanjsm', () => {
-  return del('_site/**/*.jsm')
-})
+  return del('_site/**/*.jsm');
+});
 
 gulp.task('move', () => {
   return gulp
     .src('./_site/_minjs/')
-    .pipe(gulp.dest('./_site/'))
-})
+    .pipe(gulp.dest('./_site/'));
+});
 
 gulp.task('sass', () => {
   return gulp
     .src('./_site/assets/css/*.css')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./_site/assets/css/'))
-})
+    .pipe(gulp.dest('./_site/assets/css/'));
+});
 
 // Task for building blog when something changed:
-gulp.task('jekyll',           shell.task('bundle exec jekyll build'))
-gulp.task('serve',            shell.task('bundle exec jekyll serve --profile --trace --host=localhost --config _config.yml,_config.dev.yml'))
-gulp.task('serveIncremental', shell.task('bundle exec jekyll serve --profile --trace --host=localhost --config _config.yml,_config.dev.yml --incremental '))
-gulp.task('default', gulp.series('jekyll', 'sass', 'minify', 'minjs', 'cleanjs', 'rename', 'cleanjsm'))
+gulp.task('jekyll',           shell.task('bundle exec jekyll build'));
+gulp.task('serve',            shell.task('bundle exec jekyll serve --profile --trace --host=localhost --config _config.yml,_config.dev.yml'));
+gulp.task('serveIncremental', shell.task('bundle exec jekyll serve --profile --trace --host=localhost --config _config.yml,_config.dev.yml --incremental '));
+gulp.task('default', gulp.series('jekyll', 'sass', 'minify', 'minjs', 'cleanjs', 'rename', 'cleanjsm'));
