231ecf9ac1b9cd2d49b536d69d8a65776cd8a7fd
dannygarcia <dannyboygarcia@gmail.com>
2023-10-31 17:42:21 +0800

    highlight needs possible RB

--- scripts/findNeedsRB.js
+++ scripts/findNeedsRB.js
@@ -12,44 +12,37 @@ const analyzeFile = (filePath) => {
   const content = fs.readFileSync(filePath, "utf-8");
   const verdictMatch = content.match(/^verdict:\s*(.*)$/m);
   const metaMatch = content.match(/^meta:\s*(.*)$/m);
+  const standaloneDateMatch = content.match(/^date:\s*(\d{4}-\d{2}-\d{2})/m);
 
-  // Only proceed if verdict is one of the required values
+  let standaloneDate = null;
+  if (standaloneDateMatch) {
+    standaloneDate = new Date(standaloneDateMatch[1]);
+  }
+
   if (verdictMatch && metaMatch) {
     const verdict = verdictMatch[1].trim();
     const meta = metaMatch[1].trim();
 
-    if (["reproducible", "nonverifiable", "ftbfs"].includes(verdict) &&
-        meta === "ok"
-      )  {
-      const reviewArchiveMatch = content.match(
-        /reviewArchive:\s*\n((?:\s*- date:.*\n)+)/
-      );
+    if (["reproducible", "nonverifiable", "ftbfs", "wip"].includes(verdict) && meta === "ok") {
+      const reviewArchiveMatch = content.match(/reviewArchive:\s*\n((?:\s*- date:.*\n)+)/);
       const updatedMatch = content.match(/updated:\s*(\d{4}-\d{2}-\d{2})/);
 
       if (reviewArchiveMatch && updatedMatch) {
         const updated = new Date(updatedMatch[1]);
-        const dates = reviewArchiveMatch[1]
-          .match(/\d{4}-\d{2}-\d{2}/g)
-          .map((date) => new Date(date));
-        const latestDate = new Date(Math.max.apply(null, dates));
+        const dates = reviewArchiveMatch[1].match(/\d{4}-\d{2}-\d{2}/g).map((date) => new Date(date));
+        let latestDate = new Date(Math.max(...dates));
+
+        if (standaloneDate && standaloneDate > latestDate) {
+          latestDate = standaloneDate;
+        }
 
         console.log(`\x1b[33m\x1b[1m${path.basename(filePath)}\x1b[0m`);
-        console.log(
-          `- the software was last updated on: ${
-            updated.toISOString().split("T")[0]
-          }`
-        );
-        console.log(
-          `- the last review was performed on: ${
-            latestDate.toISOString().split("T")[0]
-          }`
-        );
-        console.log(`- the verdict was: ${verdict}`);
+        console.log(`- the software was last updated on: ${updated.toISOString().split("T")[0]}`);
+        console.log(`- the last review was performed on: ${latestDate.toISOString().split("T")[0]}`);
+        console.log(`- the latest verdict was: ${verdict}`);
 
         if (latestDate < updated) {
-          console.log(
-            `${path.basename(filePath)} needs reproducibility testing.`
-          );
+          console.log(`\x1b[1m\x1b[36m${path.basename(filePath)} might need reproducibility testing.\x1b[0m`);
           needsRB.push(path.basename(filePath));
         }
       }
@@ -58,10 +51,7 @@ const analyzeFile = (filePath) => {
 };
 
 for (const folder of foldersToAnalyze) {
-  const folderPath = path.join(
-    "/home/dannybuntu/Work/walletScrutinyCom",
-    folder
-  );
+  const folderPath = path.join("/home/dannybuntu/Work/walletScrutinyCom", folder);
   const files = fs.readdirSync(folderPath);
 
   for (const file of files) {
