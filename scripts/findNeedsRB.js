4d90eec07534ce82dd990ea6267314bcff8772c8
Leo Wandersleb <leo@leowandersleb.de>
2023-11-02 10:11:47 -0300

    improve findNeedsRB
    * Don't check for the right file extension if all files must have the right file
      extension anyway
    * Load the yaml files as yaml instead of using regexp on the whole file. Regexp
      are versatile but also error prone as the searched string might be found
      elsewhere
    * Use `date` as the only "last reviewed" date. It should always be later than
      any `reviewArchive.date`
    * Only consider products as high priority if they had been RB in the past. We
      cannot follow up on all public source products with all their releases. For
      those we have open GitHub issues that we also should follow up on but with a
      different script
    * Highlight products that updated after our last review and are listed as RB
    * Show days since they updated instead of days from last review to update
    * name the text styles. \x1b[37m\x1b[1m isn't exactly readable

--- scripts/findNeedsRB.js
+++ scripts/findNeedsRB.js
@@ -1,46 +1,48 @@
 const fs = require("fs");
 const path = require("path");
+const {loadFromFile, dateOrEmpty} = require("./helper");
 const foldersToAnalyze = ["_android", "_hardware"];
+const fNormal = '\x1b[0m';
+const fBold = '\x1b[37m\x1b[1m';
+const fHighlight = '\x1b[1m\x1b[36m';
+const fWarn = '\x1b[1m\x1b[31m';
 
 const searchForVerificationText = () => {
   console.log(`
 ----------------------------
-🚀 \x1b[37m\x1b[1mProcessing files with "for verification" text\x1b[0m
+🚀 ${fBold}Processing files with "for verification" text${fNormal}
 ----------------------------
 `);
   const needVerification = [];
   for (const folder of foldersToAnalyze) {
     const files = fs.readdirSync(folder);
     for (const file of files) {
-      if (path.extname(file) === ".md") {
-        const filePath = path.join(folder, file);
-        const content = fs.readFileSync(filePath, "utf-8");
+      const filePath = path.join(folder, file);
+      const {header, body} = loadFromFile(filePath);
+      const {meta, verdict} = header;
 
-        const meta = content.match(/^meta:\s*(.*)$/m)[1].trim();
-        const verdict = content.match(/^verdict:\s*(.*)$/m)[1].trim();
-
-        if (content.includes("for verification") && 
-            meta === 'ok' && 
-            verdict === 'wip') {
-          const date = content.match(/^date:\s*(\d{4}-\d{2}-\d{2})/m)[1].trim();
-          needVerification.push({
-            date: date,
-            file: filePath
-          });
-        }
+      if (body.includes("for verification") && 
+          meta === 'ok' && 
+          verdict === 'wip') {
+        needVerification.push({
+          date: header.date,
+          file: filePath,
+          users: header.users,
+          ratings: header.ratings,
+        });
       }
     }
   }
-  needVerification.sort((a, b) => new Date(a.date) - new Date(b.date));
+  needVerification.sort((a, b) => a.date - b.date);
   for (const n of needVerification) {
-    console.log(`${n.date}: \x1b[1m\x1b[36m${n.file}\x1b[0m`);
+    console.log(`${dateOrEmpty(n.date)}: ${fHighlight}${n.file}${fNormal} ${n.users ? `(${n.users / 1000}k users)` : n.ratings ? `(${n.ratings} ratings)`: ''}`);
   }
 };
 
 const analyzeFiles = () => {
   console.log(`
 ----------------------------
-🚀 \x1b[37m\x1b[1mAnalyzing files which may need updating\x1b[0m
+🚀 ${fBold}Analyzing files which may need updating${fNormal}
 ----------------------------
 `);
   const needVerification = [];
@@ -48,51 +50,33 @@ const analyzeFiles = () => {
     const files = fs.readdirSync(folder);
 
     for (const file of files) {
-      if (path.extname(file) === ".md") {
-        analyzeFile(path.join(folder, file), needVerification);
-      }
+      analyzeFile(path.join(folder, file), needVerification);
     }
   }
   needVerification.sort((a, b) => a.dtDays - b.dtDays);
   for (const n of needVerification) {
-    console.log(`${n.updated.toISOString().split("T")[0]}: \x1b[1m\x1b[36m${n.file}\x1b[0m (${n.verdict}) last reviewed ${n.dtDays}d prior`);
+    console.log(`${dateOrEmpty(n.updated)}: ${fHighlight}${n.file}${fNormal} (${n.verdict === 'reproducible' ? `${fWarn}REPRODUCIBLE${fNormal} ` : n.verdict }) lacking review since ${n.dtDays} days`);
   }
 };
 
 const analyzeFile = (filePath, needVerification) => {
-  const content = fs.readFileSync(filePath, "utf-8");
-  const verdictMatch = content.match(/^verdict:\s*(.*)$/m);
-  const metaMatch = content.match(/^meta:\s*(.*)$/m);
-  const standaloneDateMatch = content.match(/^date:\s*(\d{4}-\d{2}-\d{2})/m);
+  const {header, body} = loadFromFile(filePath);
+  const {meta, date, verdict, updated} = header;
 
-  let standaloneDate = standaloneDateMatch ? new Date(standaloneDateMatch[1]) : null;
-
-  if (verdictMatch && metaMatch) {
-    const verdict = verdictMatch[1].trim();
-    const meta = metaMatch[1].trim();
-
-    if (["reproducible", "nonverifiable", "ftbfs", "wip"].includes(verdict) && meta === "ok") {
-      const reviewArchiveMatch = content.match(/reviewArchive:\s*\n((?:\s*- date:.*\n)+)/);
-      const updatedMatch = content.match(/updated:\s*(\d{4}-\d{2}-\d{2})/);
-
-      if (reviewArchiveMatch && updatedMatch) {
-        const updated = new Date(updatedMatch[1]);
-        const dates = reviewArchiveMatch[1].match(/\d{4}-\d{2}-\d{2}/g).map((date) => new Date(date));
-        let latestDate = new Date(Math.max(...dates));
-
-        if (standaloneDate && standaloneDate > latestDate) {
-          latestDate = standaloneDate;
-        }
-        const dtDays = Math.round((updated - latestDate) / 1000 / 60 / 60 / 24);
-
-        if (0 < dtDays) {
-          needVerification.push({
-            updated: updated,
-            file: filePath,
-            verdict: verdict,
-            dtDays: dtDays
-          });
-        }
+  if (meta === "ok") {
+    const wasReproducible = verdict === 'reproducible' || (
+        header.reviewArchive && 
+        header.reviewArchive.some(it => it.verdict === 'reproducible')
+      );
+    if (updated && wasReproducible) {
+      if (date < updated) {
+        const dtDays = Math.round((new Date() - updated) / 1000 / 60 / 60 / 24);
+        needVerification.push({
+          updated: updated,
+          file: filePath,
+          verdict: verdict,
+          dtDays: dtDays
+        });
       }
     }
   }
