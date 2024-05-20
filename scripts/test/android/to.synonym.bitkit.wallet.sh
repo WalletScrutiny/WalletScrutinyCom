9b917199d8acdf4606e6e236d055727d9b5ff4f4
Basanta Goswami <basantagoswami1999@gmail.com>
2024-05-20 08:06:05 +0530

    build scripts and result for to.synonym.bitkit.wallet

--- scripts/test/android/to.synonym.bitkit.wallet.sh
+++ scripts/test/android/to.synonym.bitkit.wallet.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 repo=https://github.com/synonymdev/bitkit.git
-tag="v$versionName"
+# TODO: The tag should be passed from the test.sh script, update it later.
+# That's because one can pass a different tag using revision-override .
+# For now, change the hardcoded version name to test another release
+# tag=$1
+tag="beta-release-118"
 builtApk=$workDir/app-release.apk
 
 test() {
-  podman rmi bitkit -f
+  podman rmi bitkit -f || true
   podman build \
-    --tag bitkit:latest \
+    --tag bitkit \
     --cgroup-manager cgroupfs \
     --ulimit nofile=16384:16384 \
+    --memory=6g \
     --build-arg UID=$(id -u) \
     --build-arg TAG=$tag \
-    --build-arg VERSION=$versionCode \
     --file $SCRIPT_DIR/test/android/to.synonym.bitkit.wallet.dockerfile
   podman run \
     -it \
     --volume $workDir:/mnt \
     --rm \
     -u root \
-    bitkit:latest \
+    bitkit \
     bash -c \
       'cp /Users/runner/work/1/s/android/app/build/outputs/apk/release/*.apk /mnt/'
 
