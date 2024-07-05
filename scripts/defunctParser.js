9f5dc86a76190b12f9d76cccbadd964539a696b5
Leo Wandersleb <leo@leowandersleb.de>
2024-07-05 13:02:20 -0400

    fix lint, timezone, null-keys
    parsing the defunct.yaml resulted in dates slipping due to the system
    timezone
    script added yaml keys multiple times
    script kept empty keys

--- scripts/defunctParser.js
+++ scripts/defunctParser.js
@@ -1,78 +1,80 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 const path = require('path');
+process.env.TZ = 'UTC'; // fix timezone issues
 
-function parseYamlFile(filePath) {
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    let data;
-    try {
-        data = yaml.load(fileContents);
-    } catch (e) {
-        console.error(`Failed to parse YAML file: ${e.message}. \n\nFind the problematic dates and try remove them first`);
-        process.exit(1)
+function parseYamlFile (filePath) {
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  let data;
+  try {
+    data = yaml.load(fileContents);
+  } catch (e) {
+    console.error(`Failed to parse YAML file: ${e.message}. \n\nFind the problematic dates and try remove them first`);
+    process.exit(1);
+  }
+
+  const parsedData = {};
+  for (const key in data) {
+    if (Object.hasOwn(data, key)) {
+      if (Object.hasOwn(parsedData, key)) {
+        console.warn(`Duplicate key found: ${key}. Merging entries.`);
+        parsedData[key] = [...new Set([...parsedData[key], ...data[key]])];
+      } else {
+        parsedData[key] = data[key];
+      }
     }
-    
-    const parsedData = {};
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            if (parsedData.hasOwnProperty(key)) {
-                console.warn(`Duplicate key found: ${key}. Merging entries.`);
-                parsedData[key] = [...new Set([...parsedData[key], ...data[key]])];
-            } else {
-                parsedData[key] = data[key];
-            }
-        }
-    }
-    return parsedData;
+  }
+  return parsedData;
 }
 
-function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+function formatDate (date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 }
 
-function getOldAppIds(yamlData, daysThreshold = 15) {
-    const oldAppIds = [];
-    const currentDate = new Date();
-    const dateThreshold = new Date(currentDate - daysThreshold * 24 * 60 * 60 * 1000);
-    for (const dateStr in yamlData) {
-        if (yamlData.hasOwnProperty(dateStr) && yamlData[dateStr] !== null && yamlData[dateStr].length > 0) {
-            const dateAdded = new Date(dateStr);
-            if ((currentDate - dateAdded) / (1000 * 60 * 60 * 24) >= daysThreshold) {
-                const cleanedAppIds = yamlData[dateStr].map(appId => appId.replace('_', ''));
-                oldAppIds.push(...cleanedAppIds);
-                delete yamlData[dateStr];
-            }
-        }
+function getOldAppIds (yamlData, daysThreshold = 15) {
+  const oldAppIds = [];
+  const currentDate = new Date();
+  const dateThreshold = new Date(currentDate - daysThreshold * 24 * 60 * 60 * 1000);
+  for (const dateStr in yamlData) {
+    if (Object.hasOwn(yamlData, dateStr) && yamlData[dateStr] !== null && yamlData[dateStr].length > 0) {
+      const dateAdded = new Date(dateStr);
+      if ((currentDate - dateAdded) / (1000 * 60 * 60 * 24) >= daysThreshold) {
+        const cleanedAppIds = yamlData[dateStr].map(appId => appId.replace('_', ''));
+        oldAppIds.push(...cleanedAppIds);
+        delete yamlData[dateStr];
+      }
     }
-    return { oldAppIds, dateThreshold: formatDate(dateThreshold), updatedYamlData: yamlData };
+  }
+  return { oldAppIds, dateThreshold: formatDate(dateThreshold), updatedYamlData: yamlData };
 }
 
-function formatAppIds(appIds) {
-    return appIds.join(',');
+function formatAppIds (appIds) {
+  return appIds.join(',');
 }
 
-function writeYamlFile(filePath, data) {
-    const formattedData = {};
-    for (const key in data) {
-        if (data.hasOwnProperty(key) && data[key] !== null && data[key].length > 0) {
-            formattedData[formatDate(new Date(key))] = data[key];
-        }
+function writeYamlFile (filePath, data) {
+  const formattedData = {};
+  for (const key in data) {
+    if (Object.hasOwn(data, key) && data[key] !== null && data[key].length > 0) {
+      formattedData[formatDate(new Date(key))] = data[key];
     }
+  }
 
-    let yamlStr = yaml.dump(formattedData, { lineWidth: -1, quotingType: '"', forceQuotes: false, indent: 2 });
-    yamlStr = yamlStr.replace(/"(\d{4}-\d{2}-\d{2})"/g, '$1');
+  let yamlStr = yaml.dump(formattedData, { lineWidth: -1, quotingType: '"', forceQuotes: false, indent: 2 });
+  yamlStr = yamlStr.replace(/"(\d{4}-\d{2}-\d{2})"/g, '$1');
 
-    yamlStr = yamlStr.replace(/^(\s*)-\s/gm, '- ');
+  yamlStr = yamlStr.replace(/^(\s*)-\s/gm, '- ');
 
-    fs.writeFileSync(filePath, yamlStr, 'utf8');
+  fs.writeFileSync(filePath, yamlStr, 'utf8');
 }
 
 const yamlFilePath = path.join(__dirname, '../_data/defunct.yaml');
 const data = parseYamlFile(yamlFilePath);
 
+// eslint-disable-next-line no-unused-vars
 const { oldAppIds, dateThreshold, updatedYamlData } = getOldAppIds(data);
 const formattedAppIds = formatAppIds(oldAppIds);
 
