5690a1323798e8ddc7a1f0e54092c9bd79589609
Daniel Andrei R. Garcia <dannybuntu@proton.me>
2024-10-01 15:28:52 +0800

    Added single file checker - but has a problem

--- scripts/localeChecker.mjs
+++ scripts/localeChecker.mjs
@@ -26,9 +26,12 @@ const cooldownPeriod = 60000; // 1 minute cooldown
 const filesPerCooldown = 50; // Number of files processed before triggering cooldown
 
 const countryCodes = 'us,cn,jp,in,gb,ca,br,ar,de,za,au,nz,kr,fr,ru'.split(',');
+const allCountryCodes = 'ae,af,ag,ai,al,am,ao,ar,at,au,az,bb,bd,be,bf,bg,bh,bj,bm,bn,bo,br,bs,bt,bw,by,bz,ca,cd,cg,ch,ci,cl,cm,cn,co,cr,cv,cy,cz,de,dk,dm,do,dz,ec,ee,eg,es,fi,fj,fm,fr,gb,gd,gh,gm,gr,gt,gw,gy,hk,hn,hr,hu,id,ie,il,in,is,it,jm,jo,jp,ke,kg,kh,kn,kr,kw,ky,kz,la,lb,lc,lk,lr,lt,lu,lv,md,mg,mk,ml,mm,mn,mo,mr,ms,mt,mu,mw,mx,my,mz,na,ne,ng,ni,nl,no,np,nz,om,pa,pe,pg,ph,pk,pl,pt,pw,py,qa,ro,ru,sa,sb,sc,se,sg,si,sk,sl,sn,sr,st,sv,sz,tc,td,th,tj,tm,tn,tr,tt,tw,tz,ua,ug,us,uy,uz,vc,ve,vg,vn,ye,za,zw'.split(',');
 
 let processedFiles = 0;
 let totalFiles = 0;
+let updatedFiles = [];
+let unavailableFiles = [];
 
 // Custom YAML schema to preserve date formats
 const customYamlType = new yaml.Type('!date', {
@@ -201,13 +204,35 @@ function getTodayDate() {
   const today = new Date();
   return today.toISOString().split('T')[0];
 }
+/**
+ * Log information to the removed.log file
+ * @returns {Promise<void>}
+ */
+async function writeLogFile(countriesChecked) {
+  const logContent = [
+    `Number of updated files: ${updatedFiles.length}`,
+    `Number of countries checked: ${countriesChecked}`,
+    '\nUpdated files:',
+    ...updatedFiles,
+    '\n' + '-'.repeat(50),
+    '\nFiles not available in other countries:',
+    ...unavailableFiles
+  ].join('\n');
+
+  await fs.writeFile('removedFix.log', logContent, 'utf8');
+  console.log('The script only checks for 5 top, + top 2 markets for each continent')
+  console.log('Some of the files that were not updated, could be in other countries.')
+  console.log('A more comprehensive check would list 161 countries.')
+  console.log('-------------------------------------------------------------')
+  console.log(`Log file "removedFix.log" has been created. Checked ${countriesChecked} countries.`);
+}
 
 /**
  * Process a single file
  * @param {string} filePath - The path of the file to process
  * @returns {Promise<void>}
  */
-async function processFile(filePath) {
+async function processFile(filePath, isSingleFile = false) {
   processedFiles++;
   console.log(`Processing file ${processedFiles} of ${totalFiles}: ${filePath}`);
 
@@ -257,12 +282,17 @@ async function processFile(filePath) {
     }
 
     let availableCountry = null;
-    for (const code of countryCodes) {
+    const codesToCheck = isSingleFile ? allCountryCodes : countryCodes;
+
+    console.log(`Checking availability in ${codesToCheck.length} countries...`);
+
+    for (const code of codesToCheck) {
       const available = await isAppAvailable(appId, code);
       if (available) {
         availableCountry = code;
         break;
       }
+      await new Promise(resolve => setTimeout(resolve, 100));
     }
 
     if (availableCountry) {
@@ -329,9 +359,14 @@ async function processFiles() {
   files.forEach((file) => console.log(file));
   console.log('\n');
 
-  const promises = files.map((filePath) => limit(() => processFile(filePath)));
+  const isSingleFile = totalFiles === 1;
+
+  const promises = files.map((filePath) => limit(() => processFile(filePath, isSingleFile)));
 
   await Promise.all(promises);
+
+  const countriesChecked = totalFiles === 1 ? allCountryCodes.length : countryCodes.length;
+  await writeLogFile(countriesChecked);
 }
 
 // Start processing files
