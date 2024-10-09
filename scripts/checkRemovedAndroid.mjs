0babb5e2d4e4295f059e4cf7e5f0ae23b3bc1bae
Daniel Andrei R. Garcia <dannybuntu@proton.me>
2024-10-09 20:58:14 +0800

    Incorporate modularization to checkRemovedAndroid.mjs

--- scripts/checkRemovedAndroid.mjs
+++ scripts/checkRemovedAndroid.mjs
@@ -1,72 +1,81 @@
-// This script checks every defunct app in the _android/ folder to see if it
-// returns a 404. If so, it will rename meta: defunct to meta:removed. 
-// All apps checked will be output in a .md file in the home directory.
-// You can run this in the home folder using
-// node scripts/android404VerificationScript.mjs
+// File: scripts/checkRemovedAndroid.mjs
 
+// Import necessary dependencies
 import gplay from 'google-play-scraper';
 import fs from 'fs/promises';
-import path from "path";
-import helper from "./helper.mjs";
+import path from 'path';
+import helper from './helper.mjs';
+import { isAppAvailable, cooldown, countryCodes, logCountryProgress } from './countryAvailabilityChecker.mjs';
 
 let notRemovedDefunctApps = [];
 let removedApps = [];
 
-const category = "android";
+const category = 'android';
 const folder = `_${category}/`;
 
 async function checkDefunctApps() {
   try {
+    // Filter files that have 'meta: defunct' in the front matter
     const files = await fs.readdir(folder);
-
+    const defunctFiles = [];
     for (const fileName of files) {
+      const content = helper.loadFromFile(path.join(folder, fileName));
+      if (content.header.meta === 'defunct') {
+        defunctFiles.push(fileName);
+      }
+    }
+
+    const totalDefunctFiles = defunctFiles.length;
+    const totalFiles = files.length;
+    let processedFiles = 0;
+    
+    console.log(`Starting processing of ${totalDefunctFiles} defunct files out of a total of ${totalFiles} in the directory '${folder}':`);
+
+    for (const fileName of defunctFiles) {
+      processedFiles++;
+      console.log(`Processing file ${processedFiles} of ${totalDefunctFiles}: ${fileName}`);
       const content = helper.loadFromFile(path.join(folder, fileName));
       const header = content.header;
       const appId = header.appId;
-      const appCountry = header.appCountry || 'us';
-      if (header.meta === "defunct") {
-        try {
-          await gplay.app({
-            appId: appId,
-            lang: "en",
-            country: appCountry
-          });
+      const appCountry = header.appCountry || 'US';
+      let availableCountry = null;
 
-          //console.log(`App with appId ${appId} is still available.`);
-          notRemovedDefunctApps.push(appId);
-        } catch (error) {
-          if (`${error}`.search(/404/) > -1) {
-            // Sets the meta to removed if playstore returns a 404 error.
-            header.meta = "removed";
-            helper.writeResult(folder, header, content.body);
-            removedApps.push(appId);
-          } else {
-            console.error(
-              `Error checking app with appId ${appId}: ${error.message}`
-            );
-          }
+      for (const country of countryCodes) {
+        logCountryProgress(country, true);
+        const isAvailable = await isAppAvailable(appId, country, 'android');
+        if (isAvailable) {
+          availableCountry = country;
+          break;
         }
+      }
+
+      if (availableCountry) {
+        console.log(`App ${appId} is available in country: ${availableCountry}`);
+        notRemovedDefunctApps.push(appId);
+      } else {
+        console.log(`App ${appId} is not available in any of the specified countries. Marking as removed.`);
+        header.meta = 'removed';
+        helper.writeResult(folder, header, content.body);
+        removedApps.push(appId);
       }
     }
 
     const formatOutput = (apps, title) => {
-      return `${title}:\n- ${apps.join('\n- ')}\n\nTotal Number of ${title.toLowerCase()}: ${apps.length}\n---\n`;
+      return `${title}:
+- ${apps.join('\n- ')}
+
+Total Number of ${title.toLowerCase()}: ${apps.length}\n---\n`;
     };
 
-    const output = formatOutput(notRemovedDefunctApps, "Apps that are defunct, but not removed") +
-      formatOutput(removedApps, "Apps that are removed from the Play Store");
+    const output =
+      formatOutput(notRemovedDefunctApps, 'Apps that are defunct, but not removed') +
+      formatOutput(removedApps, 'Apps that are removed from the Play Store');
 
-    fs.writeFile('list-of-processed-defunct-android.md', output, err => {
-      if (err) {
-        console.error('Error writing to file', err);
-      } else {
-        console.log('Results written to list-of-processed-defunct-android.md');
-      }
-    });
+    await fs.writeFile('list-of-processed-defunct-android.md', output, 'utf8');
+    console.log('Results written to list-of-processed-defunct-android.md');
   } catch (err) {
     console.error(`Error reading files from ${folder}: ${err.message}`);
   }
-
 }
 
 checkDefunctApps();
