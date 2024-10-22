045ad3703f21b6051ad5bb56fe5abc1aead8606d
Christian Hettich <chettich@gmail.com>
2024-10-22 18:36:34 +0200

    next try

---
+++ scripts/test/android/android-shell2.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {
+    config.android_sdk.accept_license = true;
+    config.allowUnfree = true;
+    config.allowUnsupportedSystem = true;
+  }
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gradle
+    pkgs.openjdk11     # Verwende nur JDK 11
+    pkgs.wget
+    pkgs.disorderfs
+    
+  ];
+
+  shellHook = ''
+    # Set Android SDK environment variable
+    echo "Android build environment is set up."
+  '';
+}
---
+++ scripts/test/android/de.schildbach.wallet-nix2.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure
+#! nix-shell android-shell2.nix
+
+takeUserActionCommand='echo "CTRL-D to continue";
+  bash'
+versionName=10.16
+workDir=/tmp/test_de.schildbach.wallet
+repo=https://github.com/bitcoin-wallet/bitcoin-wallet
+tag=v$versionName
+builtApk=$workDir/app/wallet/build/outputs/apk/prod/release/bitcoin-wallet-prod-release-unsigned.apk
+
+test() {
+  sortedDir="/tmp/sorted/"
+
+  echo "Setting up build directory with disorderfs..."
+  
+  # Check if the directory exists, and remove it if it does
+  if [ -d "$sortedDir" ]; then
+    echo "Directory $sortedDir exists. Deleting it."
+    rm -rf "$sortedDir"
+  fi
+
+  # Create the directory
+  mkdir "$sortedDir"
+
+  # Use disorderfs to ensure deterministic builds
+  disorderfs --sort-dirents=yes --reverse-dirents=no $workDir/app "$sortedDir"
+
+  echo "Building APK with Gradle..."
+  cd "$sortedDir"
+  
+  echo "ANDROID_HOME=$ANDROID_HOME"
+  
+  gradle \
+    --no-build-cache \
+    --no-daemon \
+    --no-parallel \
+    clean :wallet:assembleRelease
+
+  echo "Executing user action: $takeUserActionCommand"
+  $takeUserActionCommand
+}
+
+# Call the test function
+test
