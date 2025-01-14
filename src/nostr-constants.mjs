1f3d2db78c9c4c7b0213f38e1cd64049ed7aa36d
ktecho <ktecho@ktecho.com>
2025-01-14 13:33:23 +0100

    Tiny reorder

--- src/nostr-constants.mjs
+++ src/nostr-constants.mjs
@@ -1,7 +1,3 @@
-export const assetRegistrationKind = 106300; // 1063
-export const attestationKind = 303010; // 30301
-export const endorsementKind = 303020; // 30302
-
 export const explicitRelayUrls = [
   "wss://relay.primal.net/",
   "wss://relay.zapstore.dev/",
@@ -9,4 +5,9 @@ export const explicitRelayUrls = [
   "wss://relay.nostr.net/",
   "wss://relay.snort.social/",
   "wss://nostr-pub.wellorder.net/"
-]; 
+];
+
+// Attestations
+export const assetRegistrationKind = 106300; // 1063
+export const attestationKind = 303010; // 30301
+export const endorsementKind = 303020; // 30302
