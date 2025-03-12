e0be497ad854a763eac0b067d4f752ec412ed3e3
ktecho <ktecho@ktecho.com>
2025-03-12 12:15:56 +0100

    Commiting new nostr kinds used in tests

--- src/nostr-constants.mjs
+++ src/nostr-constants.mjs
@@ -8,8 +8,8 @@ export const explicitRelayUrls = [
 ];
 
 // Verifications
-export const assetRegistrationKind = 1263; // 1063
-export const verificationKind = 32301; // 30301
-export const endorsementKind = 32302; // 30302
+export const assetRegistrationKind = 1265; // 1063
+export const verificationKind = 32302; // 30301
+export const endorsementKind = 32304; // 30302
 
 export const verificationsFeatureSinceTS = 1739359140; // 2025-02-12 12:19 UTC
