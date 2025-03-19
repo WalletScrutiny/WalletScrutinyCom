93e29d44558d3654fdf3709d2860d76f02eac7fb
ktecho <ktecho@ktecho.com>
2025-03-19 12:37:55 +0100

    Using newer fake kinds to re-test the migration script again

--- src/nostr-constants.mjs
+++ src/nostr-constants.mjs
@@ -8,8 +8,8 @@ export const explicitRelayUrls = [
 ];
 
 // Verifications
-export const assetRegistrationKind = 1265; // 1063
-export const verificationKind = 32302; // 30301
-export const endorsementKind = 32304; // 30302
+export const assetRegistrationKind = 1266; // 1063
+export const verificationKind = 32303; // 30301
+export const endorsementKind = 30302;
 
 export const verificationsFeatureSinceTS = 1739359140; // 2025-02-12 12:19 UTC
