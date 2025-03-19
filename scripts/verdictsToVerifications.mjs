e24badc7e3a7af9340632db859d3f0f7823c6f19
ktecho <ktecho@ktecho.com>
2025-03-19 14:50:25 +0100

    verdictsToVerifications increase time between post to relays to 3s

--- scripts/verdictsToVerifications.mjs
+++ scripts/verdictsToVerifications.mjs
@@ -187,7 +187,7 @@ async function createNostrEvents({
         version,
         platform
     });
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise(resolve => setTimeout(resolve, 3000));
 }
 
 function parseResults(resultsString) {
