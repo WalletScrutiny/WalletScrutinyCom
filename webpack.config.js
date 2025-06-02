0596f206a41129230627006e357d4dd4cad2774e
ktecho <ktecho@ktecho.com>
2025-06-02 10:41:53 +0200

    Adding webpack bundle analyzer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   devtool: 'source-map',
@@ -80,6 +81,7 @@ module.exports = {
         ],
         blocking: true
       }
-    })
+    }),
+    // new BundleAnalyzerPlugin()
   ]
 };
