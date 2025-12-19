bf507541153d4d7ecbe51aa7005ec0627d19cd08
ktecho <ktecho@ktecho.com>
2025-12-19 20:00:14 +0100

    Adding new archived url to cards

--- assets/js/searchWallets.js
+++ assets/js/searchWallets.js
@@ -289,7 +289,7 @@ function makeCompactResultsHTML (wallet, lazyLoad) {
   }
 
   return [
-    `<a class="result-pl-inner ${wallet.meta}" onclick="window.location.href = '${analysisUrl}';" href='${analysisUrl}'>`,
+    `<a class="result-pl-inner ${wallet.meta}" onclick="window.location.href = '${wallet.archived ? '/archived/?appId=' + wallet.appId + '&platform=' + wallet.folder : analysisUrl}';" href='${wallet.archived ? '/archived/?appId=' + wallet.appId + '&platform=' + wallet.folder : analysisUrl}'>`,
       `<div class="icon-wrapper"><img src='${basePath}/images/${wallet.icon ? `wIcons/${wallet.folder}/small/${wallet.icon}` : 'noimg.svg'}' class='wallet-icon' ${lazyLoad ? 'loading="lazy"' : ''} /></div>`,
       '<span class="result-title-wrapper">',
         `<span>${wallet.altTitle || wallet.title}</span>`,
