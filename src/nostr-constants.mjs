3906d9556f95e1ee2a8bc25504715f55185a69c9
ktecho <ktecho@ktecho.com>
2026-02-19 11:34:35 +0100

    Updating nip89ClientTagD after running nip89_create.mjs with new main relay

--- src/nostr-constants.mjs
+++ src/nostr-constants.mjs
@@ -13,7 +13,7 @@ export const explicitRelayUrls = [
 
 export const wsBotPublicKey = '168b7a2cd8bb9205c3f574de540606d6f4c46717c5164f47373fdcce2b9cd335';
 
-export const nip89ClientTagD = '5985153332267';
+export const nip89ClientTagD = '7703371760017';
 
 // Verifications
 export const assetRegistrationKind = isDebugEnv() ? 1267 : 1063;
